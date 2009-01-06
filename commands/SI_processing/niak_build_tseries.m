0ddc54bb960f6e6d6363f83940e27aa7aed64f83
Pierre Bellec <pierre.bellec@gmail.com>
2009-01-06 05:35:56 +0000

    an additional option in NIAK_BUILD_TSERIES to extract all the time series in multiple regions rather than the average time series per region.

--- commands/SI_processing/niak_build_tseries.m
+++ commands/SI_processing/niak_build_tseries.m
@@ -28,11 +28,17 @@ function [tseries,std_tseries] = niak_build_tseries(vol,mask,opt)
 %           normalization to apply on the individual time series before 
 %           averaging in each ROI. See OPT in NIAK_NORMALIZE_TSERIES.
 %
+%       FLAG_ALL
+%           (boolean, default false) if FLAG_ALL is true, the time series
+%           of all voxels found in MASK>0 will be sent in TSERIES, rather
+%           than the mean time series.
+%
 % _________________________________________________________________________
 % OUTPUTS:
 %
 % TSERIES   
 %       (array) TSERIES(:,I) is the mean time series in the ROI MASK==I.
+%       In this case, STD_TSERIES is a sparse matrix full of zeros.
 %
 % STD_TSERIES   
 %       (arrays) STD_TSERIES(:,I) is the standard deviation of the time 
@@ -68,8 +74,8 @@ function [tseries,std_tseries] = niak_build_tseries(vol,mask,opt)
 %% Setting up default inputs
 opt_norm.type = 'none';
 gb_name_structure = 'opt';
-gb_list_fields = {'correction'};
-gb_list_defaults = {opt_norm};
+gb_list_fields = {'flag_all','correction'};
+gb_list_defaults = {false,opt_norm};
 niak_set_defaults
 
 %% Extracting the labels of regions and reorganizing the data 
@@ -80,11 +86,16 @@ nb_rois = length(labels_roi);
 tseries_mask = niak_vol2tseries(vol,mask>0);
 tseries_mask = niak_normalize_tseries(tseries_mask,opt.correction);
 
-tseries = zeros([size(tseries_mask,1) nb_rois]);
-std_tseries = zeros([size(tseries_mask,1) nb_rois]);
-mask_v = mask(mask>0);
-        
-for num_r = 1:nb_rois
-    tseries(:,num_r) = mean(tseries_mask(:,mask_v == labels_roi(num_r)),2);
-    std_tseries(:,num_r) = std(tseries_mask(:,mask_v == labels_roi(num_r)),0,2);
+if flag_all
+    tseries = tseries_mask;
+    std_tseries = sparse(size(tseries));
+else
+    tseries = zeros([size(tseries_mask,1) nb_rois]);
+    std_tseries = zeros([size(tseries_mask,1) nb_rois]);
+    mask_v = mask(mask>0);
+
+    for num_r = 1:nb_rois
+        tseries(:,num_r) = mean(tseries_mask(:,mask_v == labels_roi(num_r)),2);
+        std_tseries(:,num_r) = std(tseries_mask(:,mask_v == labels_roi(num_r)),0,2);
+    end
 end
