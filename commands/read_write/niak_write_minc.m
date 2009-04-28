dc0a11cb4ebcf9d69a1e230c29bad1c9bd7b669c
Pierre Bellec <pierre.bellec@gmail.com>
2009-04-28 16:19:23 +0000

    NIAK now writes MINC2 file (the only output format was MINC1 previously because of a bug in MINC2 libraries that is now fixed).

--- commands/read_write/niak_write_minc.m
+++ commands/read_write/niak_write_minc.m
@@ -215,10 +215,7 @@ else
     str_raw = [str_raw '-clobber ']; % overwrites existing files
 
     if strcmp(hdr.type,'minc2')
-        %% TAG : by default NIAK is using MINC1, this will be changed when the
-        %% internal compression of MINC2 will deal correctly with big 4D
-        %% datasets (this is not the case now...).
-        %str_raw = [str_raw '-2 ']; % Creates a minc2 file. Otherwise it will produce minc1
+        str_raw = [str_raw '-2 ']; % Creates a minc2 file. Otherwise it will produce minc1
     end
 
     if ~isempty(file_name)
