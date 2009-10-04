80232901076d149b925fe592aa14c7042ac38408
Pierre Bellec <pierre.bellec@gmail.com>
2009-10-04 15:57:17 +0000

    bug fix in the 1D local max extraction.

--- commands/SI_processing/niak_find_local_max_1d.m
+++ commands/SI_processing/niak_find_local_max_1d.m
@@ -1,4 +1,4 @@
-function [val,ind] = niak_find_local_max_1d(sig,neigh)
+function [val,ind] = niak_find_local_max_1d(x,sig,neigh)
 %
 % _________________________________________________________________________
 % SUMMARY NIAK_FIND_LOCAL_MAX_1D
@@ -7,19 +7,22 @@ function [val,ind] = niak_find_local_max_1d(sig,neigh)
 % a symmetric time window.
 %
 % SYNTAX:
-% [VAL,IND] = NIAK_FIND_LOCAL_MAX_1D(SIG,WW)
+% [VAL,IND] = NIAK_FIND_LOCAL_MAX_1D(X,SIG,WW)
 %
 % _________________________________________________________________________
 % INPUTS:
+%
+% X
+%       (vector) measure SIG(N) corresponds to absciss X(N).
 %
 % SIG
 %       (vector, size T*1) a time series, or at least a series of
 %       observation where proximity in the order list makes sense to define
 %       a neighbourhood
 %
-%       NEIGH
-%           (vector, default [0.7 1.3]) defines the local neighbourhood of
-%           a number of clusters.
+% NEIGH
+%       (vector, default [0.7 1.3]) defines the local neighbourhood of
+%       a number of clusters.
 %
 % _________________________________________________________________________
 % OUTPUTS :
@@ -60,18 +63,24 @@ function [val,ind] = niak_find_local_max_1d(sig,neigh)
 % OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 % THE SOFTWARE.
 
+if nargin < 3
+    neigh = [0.7,1.3];
+end
+
 nt = length(sig);
 
 val = [];
 ind = [];
 
 for num_t = 1:length(sig)
-    low = max(1,floor(neigh(1)*num_t));
-    up = min(length(sig),ceil(neigh(2)*num_t));
+    mask_low = x >= floor(neigh(1)*x(num_t));
+    mask_up = x <= ceil(neigh(2)*x(num_t));
+    mask = mask_low & mask_up;
     
-    if ~any(sig(num_t) < sig(low:up))
+    if any(mask)&&(~any(sig(num_t) < sig(mask)))
         
         val = [val ; sig(num_t)];
-        ind = [ind ; num_t];
+        ind = [ind ; x(num_t)];
+        
     end
 end
