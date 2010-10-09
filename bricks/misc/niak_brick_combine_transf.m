c1515681c611f295504a20afbfe024c7bcfa2757
Pierre Bellec <pierre.bellec@gmail.com>
2010-10-09 00:31:36 +0000

    a stub of a motion correction pipeline, which is fully hierarchical (within-run, within-session, between-session). Not ready to be used in any way, but close to completion.

---
+++ bricks/misc/niak_brick_combine_transf.m
@@ -0,0 +1,129 @@
+function [files_in,files_out,opt] = niak_brick_combine_transf(files_in,files_out,opt)
+% Combine affine transformations from multiple MAT files.
+%
+% SYNTAX:
+% [FILES_IN,FILES_OUT,OPT] = NIAK_BRICK_COMBINE_TRANSF(FILES_IN,FILES_OUT,OPT)
+%
+% _________________________________________________________________________
+% INPUTS:
+%
+%   FILES_IN      
+%       (cell of strings) each entry in the name of a .mat file with one
+%       variable TRANSF. The first variable can have multiple entries, i.e.
+%       TRANSF(:,:,I) is a transformation. The name of the variable can be
+%       changed, see OPT.VAR_NAME below.
+%
+%   FILES_OUT 
+%       (string) the name of a MAT file with one TRANSF variable, which is
+%       the combined transformations from all inputs, from the first to the
+%       last in that order.
+%
+%   OPT           
+%       (structure, optional) has the following fields:
+%
+%       VAR_NAME
+%           (string, default 'transf') the name of the transformation
+%           variable.
+%
+%       FLAG_TEST 
+%           (boolean, default: 0) if FLAG_TEST equals 1, the brick does not 
+%           do anything but update the default values in FILES_IN and 
+%           FILES_OUT.
+%
+%       FLAG_VERBOSE 
+%           (boolean, default 1) if the flag is 1, then the function prints 
+%           some infos during the processing.
+%
+% _________________________________________________________________________
+% OUTPUTS:
+%
+% The structures FILES_IN, FILES_OUT and OPT are updated with default
+% values. If OPT.FLAG_TEST == 0, the specified outputs are written.
+%
+% _________________________________________________________________________
+% COMMENTS:
+%
+% The TRANSF variables are standard 4*4 matrix array representation of 
+% an affine transformation [M T ; 0 0 0 1] for (y=M*x+T) 
+%
+% Copyright (c) Pierre Bellec, McConnell Brain Imaging Center,
+% Montreal Neurological Institute, McGill University, 2008.
+% Maintainer : pbellec@bic.mni.mcgill.ca
+% See licensing information in the code.
+% Keywords : affine transformation
+
+% Permission is hereby granted, free of charge, to any person obtaining a copy
+% of this software and associated documentation files (the "Software"), to deal
+% in the Software without restriction, including without limitation the rights
+% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+% copies of the Software, and to permit persons to whom the Software is
+% furnished to do so, subject to the following conditions:
+%
+% The above copyright notice and this permission notice shall be included in
+% all copies or substantial portions of the Software.
+%
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+% THE SOFTWARE.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Seting up default arguments %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+if ~iscellstr(files_in)
+    error('FILES_IN should be a cell of strings')
+end
+
+if isempty(files_out)
+    error('Please specify FILES_OUT')
+end
+
+% Setting up options
+gb_name_structure = 'opt';
+gb_list_fields = {'var_name' , 'flag_verbose' , 'flag_test' };
+gb_list_defaults = {'transf' , true           , false       };
+niak_set_defaults
+
+if flag_test
+    return
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%
+%% Combine transforms %%
+%%%%%%%%%%%%%%%%%%%%%%%%
+nb_file = length(files_in);
+
+for num_f = 1:nb_file
+    file_name = files_in{num_f};
+    if flag_verbose
+        fprintf('Reading %s ...\n',file_name);
+    end
+    data = load(file_name,var_name);
+    if num_f == 1
+        transf = data.(var_name);
+        nb_transf = size(transf,3);
+    else
+        if size(data.(var_name),3)~=1
+            error(sprintf('There should be only one transformation in %s',file_name));
+        end
+    end
+    for num_t = 1:nb_transf
+        transf = data.(var_name)*transf(:,:,num_t);
+    end
+end
+
+%%%%%%%%%%%%%%%%%%
+%% Save results %%
+%%%%%%%%%%%%%%%%%%
+if flag_verbose
+	fprintf('Saving results in %s ...\n',files_out);
+end
+eval(sprintf('%s = transf;',var_name));
+if flag_verbose
+	fprintf('Done !\n',files_out);
+end
+save(files_out,var_name);
---
+++ pipeline/niak_pipeline_motion_correction.m
@@ -0,0 +1,320 @@
+function [pipeline,opt] = niak_pipeline_motion_correction(files_in,opt)
+% Within-subject motion correction of fMRI data.
+% Correction is implemented via estimation of a rigid-body transform and 
+% spatial resampling.
+%
+% [PIPELINE,OPT] = NIAK_PIELINE_MOTION_CORRECTION(FILES_IN,OPT)
+%
+% _________________________________________________________________________
+% INPUTS:
+%
+%   FILES_IN  
+%       (structure) with the following fields (the field names <SESSION> 
+%       can be any arbitrary string) : 
+%
+%       <SESSION>   
+%           (cell of string) each entry is a file name of one fMRI dataset.
+%
+% * OPT   
+%       (structure) with the following fields:
+%
+%       LABEL
+%           (string, default '') an extra string which will be added in the
+%           output names.
+%
+%       PSOM
+%           (structure, default from the PSOM configuration file) the
+%           options of the pipeline system, see PSOM_RUN_PIPELINE. The
+%           folder for logs is not optional though, it is 'logs' located in 
+%           OPT.FOLDER_OUT
+%
+%       FOLDER_OUT 
+%           (string) The name of the folder to save all the outputs.
+%
+%       VOL_REF 
+%           (vector, default 'median') VOL_REF is the number of the volume 
+%           that will be used as target in each run. If VOL_REF is a 
+%           string, the median volume of the run of reference in each 
+%           session will be used rather than an arbitrary volume. This
+%           option superseeds the contents of OPT.PARAMETERS
+%
+%       RUN_REF 
+%           (vector, default 1) RUN_REF(NUM) is the number of the run that 
+%           will be used as target for session NUM.
+%           If RUN_REF is a single integer, the same number will be used 
+%           for all sessions.
+%
+%       SESSION_REF 
+%           (string, default first session) name of the session of
+%           reference. By default, it is the first field found in FILES_IN.
+%
+%       PARAMETERS
+%           (structure) The options of the estimation of the parameters of 
+%           the rigid-body motion. See the OPT argument of
+%           NIAK_BRICK_MOTION_PARAMETERS.
+%
+%       SUPPRESS_VOL 
+%           (integer, default 0) the number of volumes that are suppressed 
+%           at the begining of the time series.
+%           This is a good stage to get rid of "dummy scans"
+%           necessary to reach signal stabilization (that takes
+%           about 3 volumes).
+%
+%       INTERPOLATION 
+%           (string, default 'sinc') the spatial interpolation method used 
+%           for resampling.
+%           Available options : 'trilinear', 'tricubic', 
+%           'nearest_neighbour', 'sinc'.
+%
+%       FLAG_SKIP
+%          (boolean, default 0) if FLAG_SKIP == 0, the flag does not do
+%          anything. If FLAG_SKIP == 1, the motion parameters are 
+%          still estimated for quality control, but no 
+%          transformation only is actually applied in the resampling 
+%          (simple copy from source to target). This flag is usefull to get
+%          rid of motion correction in pipeline mode. The brick can still
+%          be used to suppress volumes at the begining of the run.
+%
+%       FLAG_TEST 
+%           (boolean, default: 0) if FLAG_TEST equals 1, the
+%           brick does not do anything but update the default
+%           values in FILES_IN, FILES_OUT and OPT.
+%
+%       FLAG_VERBOSE 
+%           (boolean, default: 1) If FLAG_VERBOSE == 1, write
+%           messages indicating progress.
+%
+% _________________________________________________________________________
+% OUTPUTS:
+%
+%   The structures FILES_IN, FILES_OUT and OPT are updated with default
+%   values. If OPT.FLAG_TEST == 0, the specified outputs are written.
+%
+% _________________________________________________________________________
+% SEE ALSO:
+%  NIAK_BRICK_MOTION_PARAMETERS, NIAK_DEMO_PIPELINE_MOTION_CORRECTION
+%
+% _________________________________________________________________________
+% COMMENTS
+%
+% NOTE 1: The motion correction follows a hierachical strategy :
+% Rigid-body transforms are first estimated within each run 
+% independently by registering all volumes to one single reference volume
+% in a run of reference.
+% Then, the volumes of reference within each session are coregistered with 
+% one volume of reference (of the run of reference)
+% Finally, these volumes of references (session level) are coregistered to
+% the volume of reference of the session of references.
+% The within-run, within-session and between-sessions transformation are 
+% combined.
+%
+% NOTE 2: if FLAG_SESSION == 1, the within-run transformations are not 
+% applied. 
+%
+% NOTE 3: if FLAG_SKIP==1, the motion parameters are still estimated yet
+% no correction is applied to the fMRI datasets.
+%
+% NOTE 4: A session corresponds to a single scanning session with one
+% participant (typically 1 hour) while a run is a single set of brain
+% volumes (typically around 5 minutes). The real distinction between
+% sessions and runs should be based upon size of displacements rather than
+% time : if a subject had to get out of the scanner and repositioned, the
+% runs before and after repositioning should be regarded as being collected
+% on different sessions.
+%
+% NOTE 5: The TRANSF variables are standard 4*4 matrix array representation 
+% of an affine transformation [M T ; 0 0 0 1] for (y=M*x+T) 
+% _________________________________________________________________________
+% Copyright (c) Pierre Bellec, Centre de recherche de l'institut de 
+% geriatrie de Montreal, Montreal, Canada, 2010.
+% Maintainer : pbellec@criugm.qc.ca
+% See licensing information in the code.
+% Keywords : medical imaging, motion correction, fMRI
+
+% Permission is hereby granted, free of charge, to any person obtaining a copy
+% of this software and associated documentation files (the "Software"), to deal
+% in the Software without restriction, including without limitation the rights
+% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+% copies of the Software, and to permit persons to whom the Software is
+% furnished to do so, subject to the following conditions:
+%
+% The above copyright notice and this permission notice shall be included in
+% all copies or substantial portions of the Software.
+%
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+% THE SOFTWARE.
+
+flag_gb_niak_fast_gb = true;
+niak_gb_vars
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Seting up default arguments %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% SYNTAX
+if ~exist('files_in','var')
+    error('SYNTAX: [PIPELINE,OPT] = NIAK_PIPELINE_MOTION_CORRECTION(FILES_IN,OPT).\n Type ''help niak_pipeline_motion_correction'' for more info.')
+end
+
+%% FILES_IN
+if ~isstruct(files_in)
+    error('FILES_IN should be a structure')
+end
+list_session = fieldnames(files_in);
+nb_session = length(list_session);
+nb_run = zeros([nb_session 1]);
+list_run = cell([nb_session 1]);
+for num_s = 1:nb_session        
+    nb_run(num_s) = length(files_in.(list_session{num_s}));
+end
+
+%% OPTIONS
+opt_tmp.flag_test = true;
+opt_psom.path_logs = '';
+gb_name_structure = 'opt';
+gb_list_fields   = {'psom'   , 'label'  , 'vol_ref', 'run_ref', 'session_ref'   , 'parameters', 'suppress_vol', 'interpolation', 'flag_skip', 'folder_out', 'flag_test', 'flag_verbose'};
+gb_list_defaults = {opt_psom , ''       , 'median' , 1        , list_session{1} , opt_tmp     , 0             , 'tricubic'     , false      , NaN         , false      , true          };
+niak_set_defaults
+
+if ~strcmp(opt.folder_out(end),filesep)
+    opt.folder_out = [opt.folder_out filesep];
+end
+opt.psom.path_logs = [folder_out,logs,filesep];
+label = [label '_'];
+
+%% Extension
+[path_f,name_f,ext_f] = fileparts(files_in.(list_session{1}){1});
+if isempty(path_f)
+    path_f = '.';
+end
+                
+if strcmp(ext_f,gb_niak_zip_ext)
+	[tmp,name_f,ext_f] = fileparts(name_f);
+    ext_f = cat(2,ext_f,gb_niak_zip_ext);
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Generate the targets (run) %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+pipeline = [];
+for num_s = 1:nb_session
+    session = list_session{num_s};
+    for num_r = 1:nb_run(num_s)        
+        clear files_in_tmp files_out_tmp opt_tmp
+        name_job        = sprintf('target_%s%s_run%i',label,session,num_r);
+        files_in_tmp{1} = files_in.(session){num_r};
+        files_out_tmp   = [opt.folder_out name_job ext_f];
+        if ischar(vol_ref)
+            opt_tmp.operation = 'vol = median(vol_in{1});';
+        else
+            opt_tmp.operation = sprintf('vol = vol_in{1}(:,:,:,%i);',vol_ref);
+        end   
+        pipeline = psom_add_job(pipeline,name_job,'niak_brick_math_vol',files_in_tmp,files_out_tmp,opt_tmp);
+    end
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Estimate the within-run motion parameters %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+for num_s = 1:nb_session
+    session = list_session{num_s};
+    for num_r = 1:nb_run(num_s)        
+        clear files_in_tmp files_out_tmp opt_tmp
+        name_job_target = sprintf('target_%s%s_run%i',label,session,num_r);
+        name_job = sprintf('within_run_%s%s_run%i',label,session,num_r);
+        files_in_tmp.fmri   = files_in.(session){num_r};
+        files_in_tmp.target = pipeline.(name_job_target).files_out;
+        files_out_tmp       = [opt.folder_out name_job '.mat'];
+        opt_tmp             = opt.parameters;
+        pipeline = psom_add_job(pipeline,name_job,'niak_brick_motion_parameters',files_in_tmp,files_out_tmp,opt_tmp);
+    end
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Estimate the within-session motion parameters %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+for num_s = 1:nb_session
+    session = list_session{num_s};    
+    for num_r = 1:nb_run(num_s)
+        if num_r~=run_ref
+            name_job_target    = sprintf('target_%s%s_run%i',label,session,run_ref);
+            name_job_source    = sprintf('target_%s%s_run%i',label,session,num_r);            
+            clear files_in_tmp files_out_tmp opt_tmp
+            name_job            = sprintf('within_session_%s%s_run%i',label,session,num_r);
+            files_in_tmp.fmri   = pipeline.(name_job_source).files_out;
+            files_in_tmp.target = pipeline.(name_job_target).files_out;
+            files_out_tmp       = [opt.folder_out name_job '.mat'];
+            opt_tmp             = opt.parameters;
+            pipeline = psom_add_job(pipeline,name_job,'niak_brick_motion_parameters',files_in_tmp,files_out_tmp,opt_tmp);
+        end
+    end            
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Estimate the between-session motion parameters %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+for num_s = 1:nb_session
+    session = list_session{num_s};
+    if ~strcmp(session,session_ref)        
+        name_job_target    = sprintf('target_%s%s_run%i',label,session_ref,run_ref);
+        name_job_source    = sprintf('target_%s%s_run%i',label,session,run_ref);
+        clear files_in_tmp files_out_tmp opt_tmp
+        name_job            = sprintf('between_session_%s_%s',label(1:end-1),session);
+        files_in_tmp.fmri   = pipeline.(name_job_source).files_out;
+        files_in_tmp.target = pipeline.(name_job_target).files_out;
+        files_out_tmp       = [opt.folder_out name_job '.mat'];
+        opt_tmp             = opt.parameters;
+        pipeline = psom_add_job(pipeline,name_job,'niak_brick_motion_parameters',files_in_tmp,files_out_tmp,opt_tmp);        
+    end
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Combine within-run, within-session and between-session motion %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+for num_s = 1:nb_session
+    session = list_session{num_s};
+    for num_r = 1:nb_run(num_s)
+        clear files_in_tmp files_out_tmp opt_tmp
+        name_job            = sprintf('motion_parameters_%s%s_run%i',label,session,num_r);
+        files_in_tmp{1} = pipeline.(sprintf('within_run_%s%s_run%i',label,session,num_r)).files_out;
+        if num_r~=run_ref
+            files_in_tmp{2} = pipeline.(sprintf('within_session_%s%s_run%i',label,session,num_r)).files_out;
+            nb_transf = 3;
+        else
+            nb_transf = 2;
+        end
+        if ~strcmp(session,session_ref)
+            files_in_tmp{nb_transf} = pipeline.(sprintf('between_session_%s%s',label,session)).files_out;
+        end                
+        files_out_tmp       = [opt.folder_out name_job '.mat'];
+        opt_tmp             = opt.parameters;
+        pipeline = psom_add_job(pipeline,name_job,'niak_brick_combine_transf',files_in_tmp,files_out_tmp,opt_tmp);
+    end
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Resample fMRI datasets %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+for num_s = 1:nb_session
+    session = list_session{num_s};
+    for num_r = 1:nb_run(num_s)
+        clear files_in_tmp files_out_tmp opt_tmp
+        files_in_tmp.transformation = pipeline.(sprintf('motion_parameters_%s%s_run%i',label,session,num_r)).files_out;
+        files_in_tmp.source         = files_in.(session){num_r};
+        files_in_tmp.target         = pipeline.(sprintf('target_%s%s_run%i',label,session_ref,run_ref)).files_out;
+        opt_tmp.interpolation       = opt.interpolation;
+        pipeline = psom_add_job(pipeline,name_job,'niak_brick_resample_vol',files_in_tmp,files_out_tmp,opt_tmp);
+    end
+end
+
+%%%%%%%%%%%%%%%%%%%%%%
+%% Run the pipeline %%
+%%%%%%%%%%%%%%%%%%%%%%
+if ~flag_test
+    psom_run_pipeline(pipeline,opt.psom);
+end
