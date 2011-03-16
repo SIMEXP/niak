d219d418cbec12d19bfb812858e7ad9f02c9f122
Pierre Bellec <pierre.bellec@gmail.com>
2011-03-16 18:14:00 +0000

    A bit of work on NIAK_FULL_DESIGN

--- commands/fmristat/niak_full_design.m
+++ commands/fmristat/niak_full_design.m
@@ -1,9 +1,6 @@
 function matrix_x = niak_full_design(x_cache,trend,opt)
-% _________________________________________________________________________
-% SUMMARY NIAK_FULL_DESIGN
-%
-% Contructs the full design matrix of the model (concatenates the information
-% in x_cache an trend)
+% Contructs the full design matrix of the model 
+% (concatenates the information in x_cache and trends)
 % 
 % SYNTAX:
 % [MATRIX_X] = NIAK_FULL_DESIGN(X_CACHE,TREND,OPT)
@@ -12,29 +9,29 @@ function matrix_x = niak_full_design(x_cache,trend,opt)
 % INPUTS:
 %
 % X_CACHE 
-% structure with the fields TR, X ,and W, obtained from niak_fmridesign 
+%   (structure) with fields TR, X ,and W. See NIAK_FMRI_DESIGN.
 %
 % TREND       
-% 3D array) of the temporal,spatial trends and additional confounds for 
-% every slice, obtained from niak_make_trends.
+%   (3D array) temporal, spatial trends and additional confounds for 
+%   every slice. See NIAK_MAKE_TRENDS.
 % 
 % OPT         
-%       (structure, optional) with the following fields :
+%   (structure, optional) with the following fields :
 %
-%       EXCLUDE: 
-%           is a list of frames that should be excluded from the analysis. 
-%           Default is [].
+%   EXCLUDE: 
+%       a list of frames that should be excluded from the analysis. 
+%       Default is [].
 %
-%       NUM_HRF_BASES
-%           row vector indicating the number of basis functions for the hrf 
-%           for each response, either 1 or 2 at the moment. At least one basis 
-%           functions is needed to estimate the magnitude, but two basis functions
-%           are needed to estimate the delay.
+%   NUM_HRF_BASES
+%       row vector indicating the number of basis functions for the hrf 
+%       for each response, either 1 or 2 at the moment. At least one basis 
+%       functions is needed to estimate the magnitude, but two basis functions
+%       are needed to estimate the delay.
 %     
-%       BASIS_TYPE 
-%           selects the basis functions for the hrf used for delay
-%           estimation, or whenever NUM_HRF_BASES = 2. These are convolved 
-%           with the stimulus to give the responses in Dim 3 of X_CACHE.X:
+%   BASIS_TYPE 
+%       selects the basis functions for the hrf used for delay
+%       estimation, or whenever NUM_HRF_BASES = 2. These are convolved 
+%       with the stimulus to give the responses in Dim 3 of X_CACHE.X:
 %           'taylor' - use hrf and its first derivative (components 1 and 2), or 
 %           'spectral' - use first two spectral bases (components 3 and 4 of Dim 3).
 %           Default is 'spectral'. 
@@ -42,8 +39,8 @@ function matrix_x = niak_full_design(x_cache,trend,opt)
 % OUTPUTS:
 %
 % MATRIX_X      
-%       (3D array) of the full design matrix, a different matrix for each
-%       slice
+%   (3D array) of the full design matrix, a different matrix for each
+%   slice.
 % _________________________________________________________________________
 % COMMENTS:
 %
