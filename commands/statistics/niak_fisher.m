b4781a69607447fc3c7e285eb444e1f2abaf0b61
Pierre Bellec <pierre.bellec@gmail.com>
2011-10-08 17:51:33 +0000

    A simple function for applying a Fisher transform on correlation coefficients.

---
+++ commands/statistics/niak_fisher.m
@@ -0,0 +1,50 @@
+function z = niak_fisher(r);
+% Fisher transformation of correlation coefficients.
+%
+% SYNTAX:
+% Z = NIAK_FISHER(R)
+%
+% _________________________________________________________________________
+% INPUTS:
+%
+% R
+%    (vector or matrix) some correlation coefficients
+%
+% _________________________________________________________________________
+% OUTPUTS:
+%
+% Z
+%    (same as R) the Fisher transform on R, i.e. (1/2) ln( (1+R)./(1-R) )
+%
+% _________________________________________________________________________
+% SEE ALSO:
+%
+% _________________________________________________________________________
+% COMMENTS:
+%
+% Copyright (c) Pierre Bellec, Centre de recherche de l'institut de 
+% Gériatrie de Montréal, Département d'informatique et de recherche 
+% opérationnelle, Université de Montréal, 2010-2011.
+% Maintainer : pierre.bellec@criugm.qc.ca
+% See licensing information in the code.
+% Keywords : statistics, correlation, Fisher
+
+% Permission is hereby granted, free of charge, to any person obtaining a copy
+% of this software and associated documentation files (the "Software"), to deal
+% in the Software without restriction, including without limitation the rights
+% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+% copies of the Software, and to permit persons to whom the Software is
+% furnished to do so, subject to the following conditions:
+%
+% The above copyright notice and this permission notice shall be included in
+% all copies or substantial portions of the Software.
+%
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+% THE SOFTWARE.
+
+z = 0.5 * log( (1+R)./(1-R) );
