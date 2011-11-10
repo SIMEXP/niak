0340bec1dff62ccf81a8b1bd39eb5bbec074c145
Pierre Bellec <pierre.bellec@gmail.com>
2011-11-10 19:35:23 +0000

    Minor modifs in NIAK_LOG_VERSION to make it robust to multiple languages.

--- commands/formats/niak_log_version.m
+++ commands/formats/niak_log_version.m
@@ -3,61 +3,39 @@ function versions = niak_log_version(folder_in,opt)
 %%%%%%%%%%%%%%%%%
 %%     SVN     %%
 %%%%%%%%%%%%%%%%%
+list_library = {'psom','niak','basc','bht','simus'};
 
+for num_l = 1:length(list_library)
+  lib = list_library{num_l};
   % Look for a .svn folder in 'psom' repository
-  if exist(cat(2,folder_in,'psom/.svn')) == 7
-    [status,output]=system(cat(2,'svn info ',folder_in,'psom/'));
-    idx = strfind(output,'Revision: ');
-    idx_end = strfind(output,'Node Kind:');
-    versions.svn.psom = output(idx+10:idx_end-2);
-  end
-
-  % Look for a .svn folder in 'niak' repository
-  if exist(cat(2,folder_in,'niak/.svn')) == 7
-    [status,output]=system(cat(2,'svn info ',folder_in,'niak/'));
-    idx = strfind(output,'Revision: ');
-    idx_end = strfind(output,'Node Kind:');
-    versions.svn.niak = output(idx+10:idx_end-2);
-  end
-
-  % Look for a .svn folder in 'basc' repository
-  if exist(cat(2,folder_in,'basc/.svn')) == 7
-    [status,output]=system(cat(2,'svn info ',folder_in,'basc/'));
-    idx = strfind(output,'Revision: ');
-    idx_end = strfind(output,'Node Kind:');
-    versions.svn.basc = output(idx+10:idx_end-2);
-  end
-
-  % Look for a .svn folder in 'bht' repository
-  if exist(cat(2,folder_in,'bht/.svn')) == 7
-    [status,output]=system(cat(2,'svn info ',folder_in,'bht/'));
-    idx = strfind(output,'Revision: ');
-    idx_end = strfind(output,'Node Kind:');
-    versions.svn.bht = output(idx+10:idx_end-2);
-  end
-
-  % Look for a .svn folder in 'simus' repository
-  if exist(cat(2,folder_in,'simus/.svn')) == 7
-    [status,output]=system(cat(2,'svn info ',folder_in,'simus/'));
-    idx = strfind(output,'Revision: ');
-    idx_end = strfind(output,'Node Kind:');
-    versions.svn.simus = output(idx+10:idx_end-2);
-  end
+  folder_lib = fullfile(folder_in,lib,filesep);
+  [status,output]=system(cat(2,'svn info ',folder_lib));
+  if status ~= 0
+      versions.svn.(lib) = NaN;
+  else
+      idx = strfind(output,'Revision: ');
+      idx_end = strfind(output,'Node Kind:');
+      if ~isempty(idx)&&~isempty(idx_end)
+          versions.svn.(lib) = output(idx+10:idx_end-2);
+      else
+          versions.svn.(lib) = output;
+      end
+   end
+end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%     Release number     %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-  % Look for the PSOM version release
-  if exist('psom_gb_vars') == 2
-      psom_gb_vars
-      versions.release.psom = gb_psom_version;
-  end
-
-  % Look for the NIAK version release
-  if exist('niak_gb_vars') == 2
-      niak_gb_vars
-      versions.release.niak = gb_niak_version;
-  end
-
+% Look for the PSOM version release
+if exist('psom_gb_vars') == 2
+    psom_gb_vars
+    versions.release.psom = gb_psom_version;
 end
+
+% Look for the NIAK version release
+if exist('niak_gb_vars') == 2
+    niak_gb_vars
+    versions.release.niak = gb_niak_version;
+end
+
