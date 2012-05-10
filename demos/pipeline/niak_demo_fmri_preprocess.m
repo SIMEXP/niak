e1119d4e76c5358ca3d50330aa8f8e9fccb10836
Pierre Bellec <pierre.bellec@gmail.com>
2012-05-10 20:49:09 +0000

    Bug fix in the demo niak.

--- demos/pipeline/niak_demo_fmri_preprocess.m
+++ demos/pipeline/niak_demo_fmri_preprocess.m
@@ -126,7 +126,7 @@ switch format_demo
                 
         files_in.subject1.anat                = cat(2,path_demo,filesep,'anat_subject1.mnc.gz');
         files_in.subject1.fmri.session1{1} = cat(2,path_demo,filesep,'func_motor_subject1.mnc.gz');
-        files_in.subject1.fmri.session1{2}.rest  = cat(2,path_demo,filesep,'func_rest_subject1.mnc.gz');     
+        files_in.subject1.fmri.session1{2} = cat(2,path_demo,filesep,'func_rest_subject1.mnc.gz');     
         
         files_in.subject2.anat                = cat(2,path_demo,filesep,'anat_subject2.mnc.gz');        
         files_in.subject2.fmri.session1.motor = cat(2,path_demo,filesep,'func_motor_subject2.mnc.gz');
