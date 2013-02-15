78940af0506bcbd5f0893fa39b472d250d3b0810
Pierre Bellec <pierre.bellec@gmail.com>
2013-02-15 19:53:04 +0000

    Minor update of affiliations.

--- bricks/fmri_preprocess/niak_brick_slice_timing.m
+++ bricks/fmri_preprocess/niak_brick_slice_timing.m
@@ -216,7 +216,9 @@ function [files_in,files_out,opt] = niak_brick_slice_timing(files_in,files_out,o
 % _________________________________________________________________________
 % Copyright (c) Pierre Bellec, Sebastien Lavoie-Courchesne
 % Montreal Neurological Institute, 2008-2010
-% Centre de recherche de l'institut de gériatrie de Montréal, 2010.
+% Centre de recherche de l'institut de gériatrie de Montréal, 
+% Department of Computer Science and Operations Research
+% University of Montreal, Québec, Canada, 2010-2013
 % Maintainer : pierre.bellec@criugm.qc.ca
 % See licensing information in the code.
 % Keywords : medical imaging, slice timing, fMRI
