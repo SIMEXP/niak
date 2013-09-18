db56d536b85225fc3ca01628dc1e48d0afdbb4c1
Christian Dansereau <christiandansereau@gmail.com>
2013-09-18 15:00:00 +0000

    New function to do one and two sample t-test

---
+++ commands/statistics/niak_ttest.m
@@ -0,0 +1,86 @@
+function [ttest_local,pvalues] = niak_ttest(X,Y,flag_two_sided)
+
+% Massively univariate t-tests.
+%
+% SYNTAX :
+% TTEST = NIAK_TTEST(X,Y)
+%
+% _________________________________________________________________________
+% INPUTS:
+%
+% X, Y
+%   (data array, size T*N) Each column is a series of independent 
+%   observations of a variable.
+%   if only X is provided calculation are a One-sample t-test, if X and Y
+%   are provided it<s a two sample t-test with not equal variance and size. 
+%
+% FLAG_TWO_SIDED
+%   
+% _________________________________________________________________________
+% OUTPUTS:
+%
+% TTEST
+%   (data array, size T*N) a one sample t-test of significance of the mean 
+%   of the columns of X against 0 (case where Y was not specified) OR a two
+%   sample t-test on the difference of the mean of X and Y.
+%
+% _________________________________________________________________________
+% COMMENTS:
+%
+% A positive t-test is for mean of Y greater than mean of X.
+%
+% Copyright (c) Pierre Bellec, Christian L. Dansereau
+% Centre de recherche de l'Institut universitaire de gériatrie de Montréal, 2012.
+% Maintainer : pierre.bellec@criugm.qc.ca
+% See licensing information in the code.
+% Keywords : ttest
+%
+% Permission is hereby granted, free of charge, to any person obtaining a copy
+% of this software and associated documentation files (the "Software"), to deal
+% in the Software without restriction, including without limitation the rights
+% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+% copies of the Software, and to permit persons to whom the Software is
+% furnished to do so, subject to the following conditions:
+%
+% The above copyright notice and this permission notice shall be included in
+% all copies or substantial portions of the Software.
+%
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+% THE SOFTWARE.
+
+if nargin<3
+    flag_two_sided = true;
+end
+
+if nargin == 1
+    t1 = size(X,1);
+    ttest_local = (mean(X,1))./(std(X,[],1)./sqrt(t1));
+    if flag_two_sided
+        pvalues = 2*(1-niak_cdf_t(abs(ttest_local),t1-1)); % two-tailed p-value          
+    else
+        pvalues = niak_cdf_t(ttest_local,t1-1); % one-tailed p-value  
+    end
+else
+    t1 = size(X,1);
+    t2 = size(Y,1);
+    s1 = std(X,[],1); % unbiased variance estimatorof X
+    s2 = std(Y,[],1); % unbiased variance estimatorof Y
+    ttest_local = (mean(Y,1)-mean(X,1))./sqrt( (s1.^2)./t1 + (s2.^2)./t2 );
+    if nargout == 2
+        df = ( ((s1.^2)./t1 + (s2.^2)./t2).^(2) ) ./ ( ((s1.^2)./t1 ).^(2) ./ (t1-1) + ((s2.^2)./t2 ).^(2) ./ (t2-1));
+        if flag_two_sided
+            pvalues = 2*(1-niak_cdf_t(abs(ttest_local),df)); % two-tailed p-value 
+        else
+            pvalues = niak_cdf_t(ttest_local,df); % one-tailed p-value  
+        end
+    end
+    
+end
+
+ttest_local(isnan(ttest_local)) = 0;
+    
