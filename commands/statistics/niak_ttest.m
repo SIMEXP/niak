50c8dcf91c2fc5262083f2169d4c96bfe403827a
Christian Dansereau <christiandansereau@gmail.com>
2013-10-07 20:00:50 +0000

    add more documentation to the function + fix a sign bug in the two sample t-test

--- commands/statistics/niak_ttest.m
+++ commands/statistics/niak_ttest.m
@@ -1,9 +1,9 @@
-function [ttest_local,pvalues] = niak_ttest(X,Y,flag_two_sided)
+function [ttest_local,pvalues] = niak_ttest(X,Y,flag_two_tailed)
 
 % Massively univariate t-tests.
 %
 % SYNTAX :
-% TTEST = NIAK_TTEST(X,Y)
+% [TTEST_LOCAL,PVALUES] = NIAK_TTEST(X,Y,FLAG_TWO_TAILED)
 %
 % _________________________________________________________________________
 % INPUTS:
@@ -12,24 +12,29 @@ function [ttest_local,pvalues] = niak_ttest(X,Y,flag_two_sided)
 %   (data array, size T*N) Each column is a series of independent 
 %   observations of a variable.
 %   if only X is provided calculation are a One-sample t-test, if X and Y
-%   are provided it<s a two sample t-test with not equal variance and size. 
+%   are provided it's a two sample t-test with none equal variance and size. 
 %
-% FLAG_TWO_SIDED
+% FLAG_TWO_TAILED
 %   
 % _________________________________________________________________________
 % OUTPUTS:
 %
-% TTEST
+% TTEST_LOCAL
 %   (data array, size T*N) a one sample t-test of significance of the mean 
 %   of the columns of X against 0 (case where Y was not specified) OR a two
 %   sample t-test on the difference of the mean of X and Y.
+%
+% PVALUES
+%   (data array, size T*N) the probability of obtaining a test statistic at 
+%   least as extreme as the one that was actually observed, assuming that 
+%   the null hypothesis is true.
 %
 % _________________________________________________________________________
 % COMMENTS:
 %
 % A positive t-test is for mean of Y greater than mean of X.
 %
-% Copyright (c) Pierre Bellec, Christian L. Dansereau
+% Copyright (c) Christian L. Dansereau, Pierre Bellec
 % Centre de recherche de l'Institut universitaire de gériatrie de Montréal, 2012.
 % Maintainer : pierre.bellec@criugm.qc.ca
 % See licensing information in the code.
@@ -54,7 +59,7 @@ function [ttest_local,pvalues] = niak_ttest(X,Y,flag_two_sided)
 % THE SOFTWARE.
 
 if nargin<3
-    flag_two_sided = true;
+    flag_two_tailed = true;
 end
 
 if nargin == 1
@@ -63,7 +68,7 @@ if nargin == 1
     nans = isnan(ttest_local);
     ttest_local(nans)=0;
     
-    if flag_two_sided
+    if flag_two_tailed
         pvalues = 2*(1-niak_cdf_t(abs(ttest_local),t1-1)); % two-tailed p-value          
     else
         pvalues = niak_cdf_t(ttest_local,t1-1); % one-tailed p-value  
@@ -76,13 +81,13 @@ else
     t2 = size(Y,1);
     s1 = std(X,[],1); % unbiased variance estimatorof X
     s2 = std(Y,[],1); % unbiased variance estimatorof Y
-    ttest_local = (mean(Y,1)-mean(X,1))./sqrt( (s1.^2)./t1 + (s2.^2)./t2 );
+    ttest_local = (mean(X,1)-mean(Y,1))./sqrt( (s1.^2)./t1 + (s2.^2)./t2 );
     nans = isnan(ttest_local);
     ttest_local(nans)=0;
     
     if nargout == 2
         df = ( ((s1.^2)./t1 + (s2.^2)./t2).^(2) ) ./ ( ((s1.^2)./t1 ).^(2) ./ (t1-1) + ((s2.^2)./t2 ).^(2) ./ (t2-1));
-        if flag_two_sided
+        if flag_two_tailed
             pvalues = 2*(1-niak_cdf_t(abs(ttest_local),df)); % two-tailed p-value 
         else
             pvalues = niak_cdf_t(ttest_local,df); % one-tailed p-value  
@@ -92,5 +97,4 @@ else
     pvalues(nans) = NaN;
 end
 
-%ttest_local(isnan(ttest_local)) = 0;
     
