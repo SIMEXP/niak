9137645106461c99ba49a08d3ad7de477740d0a8
Sebastian Urchs <sebastian.urchs@gmail.com>
2014-05-25 03:49:34 +0000

    Renaming of scale parameters TESTING

--- extensions/surfstab/niak_brick_stability_surf_msteps_part.m
+++ extensions/surfstab/niak_brick_stability_surf_msteps_part.m
@@ -15,13 +15,13 @@ function [files_in,files_out,opt] = niak_brick_stability_surf_msteps_part(files_
 %
 %   MSTEPS
 %       (string) path to the file that contains the results of the msteps
-%       process. Must contain two variables with names OPT.SCALES_FINAL and
-%       OPT.NAME_SCALES_MAX.
+%       process. Must contain two variables with names SCALES_FINAL and
+%       SCALES_MAX.
 %
 %   CONS
 %       (string) path to the file that contains the results of the
 %       consensus clustering step. Must contain a variable with name
-%       OPT.NAME_HIER.
+%       HIER.
 %
 % FILES_OUT
 %   (string) path to structure with the following fields :
@@ -32,27 +32,6 @@ function [files_in,files_out,opt] = niak_brick_stability_surf_msteps_part(files_
 %   NAME_PART_ROI
 %       (string, default: 'part_roi') the name of the variable in
 %       FILES_IN.ROI that contains the partition into rois.
-%
-%   NAME_SCALES_FINAL
-%       (string, default: 'scales_final') name of variable in
-%       FILES_IN.MSTEPS that contains an array of dimension 2 by K where K
-%       is the number of optimal scales selected by the msteps step. The
-%       first column contains the optimal target cluster scales and
-%       the second column contains the corresponding optimal stochastic
-%       cluster scales for replication
-%
-%   NAME_SCALES_MAX
-%       (string, default: 'scales_max') name of variable in FILES_IN.MSTEPS
-%       that contains an array of dimension 2 by N where N is the number of
-%       all scales that were investigated by msteps. The variable
-%       OPT.NAME_SCALES_FINAL is a subset of this array. This array is used
-%       to index the correct hierarchy.
-%
-%   NAME_HIER
-%       (string, default: 'hier')
-%
-%   NAME_STAB
-%       (string, default: 'stab')
 %
 %   FLAG_VERBOSE
 %       (boolean, default 1) if the flag is 1, then the function
@@ -72,15 +51,20 @@ function [files_in,files_out,opt] = niak_brick_stability_surf_msteps_part(files_
 % OUTPUT VARIABLES:
 %   PART
 %       (array) the target partitions for the desired scales in
-%       FILES_IN.MSTEPS OPT.NAME_SCALES_FINAL. The output array will have
+%       FILES_IN.MSTEPS SCALES_FINAL. The output array will have
 %       dimensions V by K where V is the number of verteces on the surface
-%       and K is the number of scales.
+%       and K is the number of target scales and corresponds to the order
+%       in SCALE_TAR.
 %
-%   SCALE
+%   SCALE_REP
 %       (vector) the vector with the stochastic scales K used for
 %       replication of the target clusters in SCALE_TARGET
 %
-%   SCALE_TARGET
+%   SCALE_GRID
+%       (vector) identical to scale_rep. This is saved to conform with
+%       naming conventions
+%
+%   SCALE_TAR
 %       (vector) the vector with the scales of the target clusters.
 %
 %   PART_ROI
@@ -88,6 +72,10 @@ function [files_in,files_out,opt] = niak_brick_stability_surf_msteps_part(files_
 %
 %   HIER
 %       (cell array) containing the hierarchy corresponding
+%
+%   STAB
+%       (array) vectorized stability map of dimensions S by K where K is
+%       the number of scales in SCALE_REP that they were generated with
 %
 % _________________________________________________________________________
 % SEE ALSO:
@@ -139,8 +127,8 @@ if ~ischar(files_out)
 end
 
 %% Options
-list_fields   = { 'name_part_roi' , 'name_scales_final' , 'name_scales_max' , 'name_hier' , 'name_stab' , 'flag_verbose' , 'flag_test' };
-list_defaults = { 'part_roi'      , 'scales_final'      , 'scales_max'      , 'hier'      , 'stab'      , true           , false       };
+list_fields   = { 'name_part_roi' , 'flag_verbose' , 'flag_test' };
+list_defaults = { 'part_roi'      , true           , false       };
 opt = psom_struct_defaults(opt,list_fields,list_defaults);
 
 if opt.flag_test
@@ -148,64 +136,62 @@ if opt.flag_test
 end
 
 %% Reading all inputs
-% TODO:
-% - add opt.name_scales_max to the checks!
-%
-% Checking the contents of the files, one input per file!
 missing = false(3,1);
-fields = {opt.name_hier, opt.name_part_roi, opt.name_scales_final};
+fields = {'hier', opt.name_part_roi, 'scales_final'};
 inputs = {files_in.cons, files_in.roi, files_in.msteps};
 
-cont_cons = whos('-file',files_in.cons);
-if ~ismember(opt.name_hier, {cont_cons.name})
+cont_cons = whos('-file', files_in.cons);
+if ~ismember('hier', {cont_cons.name})
     missing(1) = true;
 end
 
-roi_cons = whos('-file',files_in.roi);
+roi_cons = whos('-file', files_in.roi);
 if ~ismember(opt.name_part_roi, {roi_cons.name})
     missing(2) = true;
 end
 
-mstep_cons = whos('-file',files_in.msteps);
-if ~ismember(opt.name_scales_final, {mstep_cons.name})
-    missing(3) = false;
+mstep_cons = whos('-file', files_in.msteps);
+if ~ismember('scales_final', {mstep_cons.name})
+    missing(3) = true;
 end
 
 if any(missing)
     error('I couln''t find %s in %s.\n', fields(missing), inputs(missing));
 else
     % Loading inputs
-    cons = load(files_in.cons, opt.name_hier);
-    hier = cons.(opt.name_hier);
+    cons = load(files_in.cons, 'hier');
+    hier = cons.hier;
     
     roi = load(files_in.roi, opt.name_part_roi);
     part_roi = roi.(opt.name_part_roi);
     
-    msteps = load(files_in.msteps, opt.name_scales_final,...
-                  opt.name_scales_max, opt.name_stab);
-    scales_final = msteps.(opt.name_scales_final);
-    scales_max = msteps.(opt.name_scales_max);
+    msteps = load(files_in.msteps, 'scales_final', 'scales_max', 'stab');
+    scale_final = msteps.scales_final;
+    scale_max = msteps.scales_max;
 end
 
 %% Generating the mstep based partition
-num_scales = size(scales_final, 1);
+num_scales = size(scale_final, 1);
 % Get the optimal scales for the final clusters
-scales_target = scales_final(:, 2);
+scale_tar = scale_final(:, 2);
 % Get the corresponding optimal scales of the stochastic clusters that were
 % used to generate the stability matrices
-scales_stoc = scales_final(:, 1);
+scale_rep = scale_final(:, 1);
 % Get the index of the stochastic scales so we know which hierarchy to pick up
-k_ind = arrayfun(@(x) find(scales_max(:, 1) == x,1,'first'), scales_stoc);
+k_ind = arrayfun(@(x) find(scale_max(:, 1) == x,1,'first'), scale_rep);
 
 V = size(part_roi, 1);
 part = zeros(V, num_scales);
 
 for sc_id = 1:num_scales
-    stoc_ind = k_ind(sc_id);
-    sc_clust = scales_target(sc_id);
-    sc_hier = hier{stoc_ind};
+    rep_ind = k_ind(sc_id);
+    % Get the current target scale
+    sc_tar = scale_tar(sc_id);
+    % Pick up the hierarchy for the stability map associated with the
+    % optimal replication scale for sc_tar
+    sc_hier = hier{rep_ind};
 
-    opt_t.thresh = sc_clust;
+    opt_t.thresh = sc_tar;
 
     tmp_part = niak_threshold_hierarchy(sc_hier,opt_t);
     % Bring partition into vertex space
@@ -213,13 +199,14 @@ for sc_id = 1:num_scales
 end
 
 % Truncate the stability matrix to the selected stochastic scales
-tmp_stab = msteps.(opt.name_stab);
+tmp_stab = msteps.stab;
 stab = tmp_stab(:, k_ind);
 hier = hier(k_ind);
+scale_grid = scale_rep;
 
 %% Save outputs
 if opt.flag_verbose
     fprintf('Saving outputs in a mat file at %s\n',files_out);
 end
-scale = scales_stoc;
-save(files_out, 'part', 'scale', 'scales_target', 'part_roi', 'hier', 'stab');    
+save(files_out, 'part', 'scale_rep', 'scale_tar', 'part_roi', 'hier',...
+     'stab', 'scale_grid');    
