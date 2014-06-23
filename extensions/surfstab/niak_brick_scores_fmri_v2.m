37a98bb22b285aaabb9b6711ff296fb65cf80238
Pierre Bellec <pierre.bellec@gmail.com>
2014-06-23 08:07:49 +0000

    Added the ability to generate correlation maps and dual regression maps in the scores brick.

--- extensions/surfstab/niak_brick_scores_fmri_v2.m
+++ extensions/surfstab/niak_brick_scores_fmri_v2.m
@@ -26,6 +26,12 @@ function [in,out,opt] = niak_brick_scores_fmri_v2(in,out,opt)
 %   in a cluster.
 % FILES_OUT.EXTRA
 %   (string) extra info in a .mat file.
+% FILES_OUT.RMAP_PART
+%   (string) correlation maps using the partition FILES_IN.PART as seeds.
+% FILES_OUT.RMAP_CORES
+%   (string) correlation maps using the partition FILES_OUT.PARTITION_CORES as seeds.
+% FILES_OUT.DUAL_REGRESSION
+%   (string) the "dual regression" maps using FILES_IN.PART as seeds.
 %
 % OPT.FOLDER_OUT (string, default empty) if non-empty, use that to generate default results.
 % OPT.NB_SAMPS (integer, default 100) the number of replications to 
@@ -51,6 +57,10 @@ function [in,out,opt] = niak_brick_scores_fmri_v2(in,out,opt)
 % COMMENTS:
 % For "window" sampling, the OPT.NB_SAMPS argument is ignored, 
 % and all possible sliding windows are generated.
+%
+% If OPT.FOLDER_OUT is specified, by default all outputs are generated. 
+% Otherwise, no output is generated. Individual outputs can be turned on/off
+% by assigning the output the value 'gb_niak_omitted'
 %
 % Copyright (c) Pierre Bellec,
 %   Centre de recherche de l'institut de Geriatrie de Montreal
@@ -106,12 +116,12 @@ if ~isempty(opt.folder_out)
     path_out = niak_full_path(opt.folder_out);
     [~,~,ext] = niak_fileparts(in.fmri);
     out = psom_struct_defaults(out, ...
-            { 'partition_cores'                , 'stability_maps'                , 'stability_intra'                , 'stability_inter'                , 'stability_contrast'                , 'partition_thresh'                , 'extra'                }, ...
-            { [path_out 'partition_cores' ext] , [path_out 'stability_maps' ext] , [path_out 'stability_intra' ext] , [path_out 'stability_inter' ext] , [path_out 'stability_contrast' ext] , [path_out 'partition_thresh' ext] , [path_out 'extra.mat'] });
+            { 'partition_cores'                , 'stability_maps'                , 'stability_intra'                , 'stability_inter'                , 'stability_contrast'                , 'partition_thresh'                , 'extra'                , 'rmap_part'                , 'rmap_cores'                , 'dual_regression'                }, ...
+            { [path_out 'partition_cores' ext] , [path_out 'stability_maps' ext] , [path_out 'stability_intra' ext] , [path_out 'stability_inter' ext] , [path_out 'stability_contrast' ext] , [path_out 'partition_thresh' ext] , [path_out 'extra.mat'] , [path_out 'rmap_part' ext] , [path_out 'rmap_cores' ext] , [path_out 'dual_regression' ext] });
 else
     out = psom_struct_defaults(out, ...
-            { 'partition_cores' , 'stability_maps' , 'stability_intra' , 'stability_inter' , 'stability_contrast' , 'partition_thresh' , 'extra' }, ...
-            { NaN               , NaN              , NaN               , NaN               , NaN                  , NaN                , NaN     });
+            { 'partition_cores' , 'stability_maps'  , 'stability_intra' , 'stability_inter' , 'stability_contrast' , 'partition_thresh' , 'extra'           , 'rmap_part'       , 'rmap_cores'      , 'dual_regression' }, ...
+            { 'gb_niak_omitted' , 'gb_niak_omitted' , 'gb_niak_omitted' , 'gb_niak_omitted' , 'gb_niak_omitted'    , 'gb_niak_omitted'  , 'gb_niak_omitted' , 'gb_niak_omitted' , 'gb_niak_omitted' , 'gb_niak_omitted' });
 end
 
 % If the test flag is true, stop here !
@@ -139,8 +149,7 @@ part_v = part(mask);
 for rr = 1:length(in.fmri)
     if rr>1
         [hdr,vol] = niak_read_vol(in.fmri{rr});
-    end    
-    tseries_r = niak_vol2tseries(vol,mask);
+    end        
     if rr == 1
          tseries = niak_normalize_tseries(niak_vol2tseries(vol,mask));
     else
@@ -152,51 +161,109 @@ end
 opt_score = rmfield(opt,{'folder_out','thresh','rand_seed','flag_test'});
 res = niak_stability_scores_v2(tseries,part_v,opt_score);
 
-if opt.flag_verbose
-    fprintf('Writing stability maps\n')
+if ~strcmp(out.stability_maps,'gb_niak_omitted')
+    if opt.flag_verbose
+        fprintf('Writing stability maps\n')
+    end
+    stab_maps = niak_part2vol(res.stab_maps',mask);
+    hdr.file_name = out.stability_maps;
+    niak_write_vol(hdr,stab_maps);
 end
-stab_maps = niak_part2vol(res.stab_maps',mask);
-hdr.file_name = out.stability_maps;
-niak_write_vol(hdr,stab_maps);
 
-if opt.flag_verbose
-    fprintf('Writing intra-cluster stability\n')
+if ~strcmp(out.stability_intra,'gb_niak_omitted')
+    if opt.flag_verbose
+        fprintf('Writing intra-cluster stability\n')
+    end
+    stab_intra = niak_part2vol(res.stab_intra',mask);
+    hdr.file_name = out.stability_intra;
+    niak_write_vol(hdr,stab_intra);
 end
-stab_intra = niak_part2vol(res.stab_intra',mask);
-hdr.file_name = out.stability_intra;
-niak_write_vol(hdr,stab_intra);
 
-if opt.flag_verbose
-    fprintf('Writing inter-cluster stability\n')
+if ~strcmp(out.stability_inter,'gb_niak_omitted')
+    if opt.flag_verbose
+        fprintf('Writing inter-cluster stability\n')
+    end
+    stab_inter = niak_part2vol(res.stab_inter',mask);
+    hdr.file_name = out.stability_inter;
+    niak_write_vol(hdr,stab_inter);
 end
-stab_inter = niak_part2vol(res.stab_inter',mask);
-hdr.file_name = out.stability_inter;
-niak_write_vol(hdr,stab_inter);
 
-if opt.flag_verbose
-    fprintf('Writing stability contrast\n')
+if ~strcmp(out.stability_contrast,'gb_niak_omitted')
+    if opt.flag_verbose
+        fprintf('Writing stability contrast\n')
+    end
+    stab_contrast = niak_part2vol(res.stab_contrast',mask);
+    hdr.file_name = out.stability_contrast;
+    niak_write_vol(hdr,stab_contrast);
 end
-stab_contrast = niak_part2vol(res.stab_contrast',mask);
-hdr.file_name = out.stability_contrast;
-niak_write_vol(hdr,stab_contrast);
 
-if opt.flag_verbose
-    fprintf('Writing partition based on cores\n')
+if ~strcmp(out.partition_cores,'gb_niak_omitted')
+    if opt.flag_verbose
+        fprintf('Writing partition based on cores\n')
+    end
+    part_cores = niak_part2vol(res.part_cores',mask);
+    hdr.file_name = out.partition_cores;
+    niak_write_vol(hdr,part_cores);
 end
-part_cores = niak_part2vol(res.part_cores',mask);
-hdr.file_name = out.partition_cores;
-niak_write_vol(hdr,part_cores);
 
-if opt.flag_verbose
-    fprintf('Writing partition based on cores, thresholded on stability\n')
+if ~strcmp(out.partition_thresh,'gb_niak_omitted')
+    if opt.flag_verbose
+        fprintf('Writing partition based on cores, thresholded on stability\n')
+    end
+    hdr.file_name = out.partition_thresh;
+    part_cores(stab_contrast<opt.thresh) = 0;
+    niak_write_vol(hdr,part_cores);
 end
-hdr.file_name = out.partition_thresh;
-part_cores(stab_contrast<opt.thresh) = 0;
-niak_write_vol(hdr,part_cores);
 
-if opt.flag_verbose
-    fprintf('Writing extra info\n')
+if ~strcmp(out.extra,'gb_niak_omitted')
+    if opt.flag_verbose
+        fprintf('Writing extra info\n')
+    end
+    nb_iter = res.nb_iter;
+    changes = res.changes;
+    save(out.extra,'nb_iter','changes')
 end
-nb_iter = res.nb_iter;
-changes = res.changes;
-save(out.extra,'nb_iter','changes')
+
+if ~strcmp(out.rmap_part,'gb_niak_omitted')
+    if opt.flag_verbose
+        fprintf('Writing correlation maps (seed: initial partition)\n')
+    end
+    opt_t.type_center = 'mean';
+    opt_t.correction = 'mean_var';
+    tseed = niak_build_tseries(tseries,part_v,opt_t);
+    rmap = niak_part2vol(niak_fisher(corr(tseries,tseed))',mask);
+    hdr.file_name = out.rmap_part;   
+    niak_write_vol(hdr,rmap);  
+end
+
+if ~strcmp(out.rmap_cores,'gb_niak_omitted')
+    if opt.flag_verbose
+        fprintf('Writing correlation maps (seed: cores)\n')
+    end
+    opt_t.type_center = 'mean';
+    opt_t.correction = 'mean_var';
+    tseed = niak_build_tseries(tseries,res.part_cores,opt_t);
+    rmap = niak_part2vol(niak_fisher(corr(tseries,tseed))',mask);
+    hdr.file_name = out.rmap_cores;   
+    niak_write_vol(hdr,rmap);  
+end
+
+if ~strcmp(out.dual_regression,'gb_niak_omitted')
+    if opt.flag_verbose
+        fprintf('Writing dual regression maps \n')
+    end
+    opt_t.type_center = 'mean';
+    opt_t.correction = 'mean_var';
+    tseed = niak_build_tseries(tseries,part_v,opt_t);
+    tseed = niak_normalize_tseries(tseed);
+    tseries = niak_normalize_tseries(tseries);
+    try
+        beta = niak_lse(tseries,tseed);
+    catch 
+        warning('Dual regression was ill-conditioned')
+        beta = zeros(size(tseries,1),max(part_v));
+    end
+    beta = niak_part2vol(beta,mask);
+    hdr.file_name = out.dual_regression;
+    niak_write_vol(hdr,beta);
+end
