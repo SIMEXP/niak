adbf1cfdbc2bbbcb0e20294168ba53679f48de5a
Sebastian <sebastian.urchs@gmail.com>
2014-10-18 16:03:29 -0400

    test

--- demos/run_test.m
+++ demos/run_test.m
@@ -1,5 +1,6 @@
 addpath(genpath('niak'));
 addpath(genpath('psom'));
 fprintf('Running super cool test now\n');
+fprintf(sprintf('%s\n',pwd));
 niak_test_all (struct, struct('flag_test', true));
 fprintf('\nJust ran supercool test\n');
