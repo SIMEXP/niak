b7c1484ebd2b59293143eb59e073973a6f9b4c53
Sebastian <surchs@users.noreply.github.com>
2014-12-01 12:58:36 -0500

    changes 9

--- demos/more_test.sh
+++ demos/more_test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 # Add the certificate for the backports
-gpg --keyserver pgpkeys.mit.edu --recv-key  8B48AD6246925553      
-gpg -a --export 8B48AD6246925553 | sudo apt-key add -
+sudo gpg --keyserver pgpkeys.mit.edu --recv-key  8B48AD6246925553      
+sudo gpg -a --export 8B48AD6246925553 | sudo apt-key add -
