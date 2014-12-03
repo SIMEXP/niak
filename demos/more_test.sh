dacd31cd659e31a0ccb0444c25e74454f7254fdb
surchs <sebastian.urchs@gmail.com>
2014-12-03 01:13:05 -0500

    clean up things

--- demos/more_test.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# Add the certificate for the backports
-sudo gpg --keyserver pgpkeys.mit.edu --recv-key  8B48AD6246925553      
-sudo gpg -a --export 8B48AD6246925553 | sudo apt-key add -
--- demos/run_test.m
+++
@@ -1,5 +0,0 @@
-addpath(genpath(pwd));
-fprintf('Running super cool test now\n');
-fprintf(sprintf('%s\n',pwd));
-niak_test_all (struct, struct('flag_test', true));
-fprintf('\nJust ran supercool test\n');
