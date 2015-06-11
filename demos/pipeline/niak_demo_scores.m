546a7f24ca6e0bc612b04275819791835a1f9ca3
surchs <sebastian.urchs@gmail.com>
2015-06-11 13:28:15 -0400

    nothin done

--- demos/pipeline/niak_demo_scores.m
+++ demos/pipeline/niak_demo_scores.m
@@ -87,8 +87,8 @@ if nargin < 2
 end
 
 path_demo = niak_full_path(path_demo);
-opt = psom_struct_defaults(opt,{'files_in','folder_out'                        ,'flag_test'},...
-						       {''        ,[path_demo,filesep,'scores',filesep],false      });
+opt = psom_struct_defaults(opt,{'files_in','folder_out'                        ,'flag_test' },...
+						       {''        ,[path_demo,filesep,'scores',filesep],false       });
 
 if isempty(opt.files_in)&&~strcmp(opt.files_in,'gb_niak_omitted')
     %% Grab the results from the NIAK fMRI preprocessing pipeline
