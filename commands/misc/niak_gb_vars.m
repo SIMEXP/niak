54f639b368a4627841fdb6fa7d7a5736dbbbb200
Pierre-Olivier Quirion <pioliqui@gmail.com>
2016-03-16 15:33:13 -0400

    "Updated target name"

--- commands/misc/niak_gb_vars.m
+++ commands/misc/niak_gb_vars.m
@@ -143,7 +143,7 @@ gb_niak_ps2pdf = 'ps2pdf';
 gb_niak_version = '0.13.5';
 
 %% Target for tests
-gb_niak_target_test = '6.6.6';
+gb_niak_target_test = '33.33.33';
 
 %% In which path is NIAK ?
 str_read_vol = which('niak_read_vol');
