4156c7bfc4a0b801d3aa10670be168b9de023188
angela-tam <angela.tam08@gmail.com>
2016-04-08 19:37:30 -0400

    first brick for subtype

---
+++ extensions/subtype/niak_brick_subtype_similarity_matrix.m
@@ -0,0 +1,94 @@
+function [files_in,files_out,opt] = niak_brick_subtype_similarity_matrix(files_in,files_out,opt)
+% Build similarity matrix for subtype pipeline
+% 
+% SYNTAX:
+% [FILE_IN,FILE_OUT,OPT] = NIAK_BRICK_subtype_similarity_matrix(FILE_IN,FILE_OUT,OPT)
+% _________________________________________________________________________
+% 
+% INPUTS:
+% 
+% FILES_IN 
+%       (string) path to a .mat file containing an array (#subjects x 
+%       #vertices OR voxels OR regions) generated from subtype_preprocessing  
+% 
+% FILES_OUT 
+%       (string) path for results (default pwd)
+% 
+% OPT 
+%       (structure, optional) with the following fields:
+% 
+%   FLAG_VERBOSE
+%       (boolean, default true) turn on/off the verbose.
+%
+%   FLAG_TEST
+%       (boolean, default false) if the flag is true, the brick does not do 
+%       anything but updating the values of FILES_IN, FILES_OUT and OPT.
+% _________________________________________________________________________
+% OUTPUTS:
+%
+% The structures FILES_IN, FILES_OUT and OPT are updated with default
+% valued. If OPT.FLAG_TEST == 0, the specified outputs are written.
+
+%% Initialization and syntax checks
+
+% Syntax
+if ~exist('files_in','var')
+    error('niak:brick','syntax: [FILES_IN,FILES_OUT,OPT] = NIAK_BRICK_SUBTYPE_SIMILARITY_MATRIX(FILES_IN,FILES_OUT,OPT).\n Type ''help niak_brick_subtype_similarity_matrix'' for more info.')
+end
+
+% Input
+if ~ischar(files_in)
+    error('FILES_IN should be a string');
+end
+
+% Output
+if ~exist('files_out','var')||isempty(files_out)
+    files_out = pwd;
+end
+
+% Options
+if nargin < 3
+    opt = struct;
+end
+
+list_fields   = { 'flag_verbose' , 'flag_test' };
+list_defaults = { true           , false       };
+opt = psom_struct_defaults(opt,list_fields,list_defaults);
+
+% If the test flag is true, stop here !
+if opt.flag_test == 1
+    return
+end
+
+%% Read the data
+data = load(files_in);
+
+%% Build correlation matrix
+R = niak_build_correlation(data.data');
+
+% Cluster subjects
+hier = niak_hierarchical_clustering(R);
+
+% Reorder subjects based on clustering
+order = niak_hier2order(hier);
+
+% Generate re-ordered matrix
+simmat = R(order,order);
+
+%% Save the similarity matrix
+
+% Save as .mat file
+
+%%% files_out subfolder??
+mat_file = strcat(files_out,'_similarity_matrix.mat');
+save(mat_file,'simmat');
+
+% Save as .png file
+opt_png.limits = [-1 1];
+opt_png.color_map = 'hot_cold';
+niak_visu_matrix(simmat,opt_png);
+namefig = 'similarity_matrix.png';
+print(namefig,'-dpng','-r300');
+
+end
+
