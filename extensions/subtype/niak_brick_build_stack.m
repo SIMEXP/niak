f5bbd06a60524a6fd036b23a0947a6872b49eac2
yassinebha <yanamarji@gmail.com>
2016-04-13 17:06:05 -0400

    WinP: changing to vector stack instead of volume

--- extensions/subtype/niak_brick_build_stack.m
+++ extensions/subtype/niak_brick_build_stack.m
@@ -13,15 +13,19 @@ function [files_in,files_out,opt] = niak_brick_build_stack(files_in,files_out,op
 %       (string) Containing the individual map (e.g. rmap_part,stability_maps, etc)
 %       NB: assumes there is only 1 .nii.gz or mnc.gz map per individual.
 %
+%   MASK
+%       (3D volume) a binary mask of the voxels that will be included in the 
+%       time*space array.
+%
 %   PHENO
 %       (strings,Default '') a .csv files coding for the pheno
-
+%
 %
 % FILES_OUT (string) the full path for a load_stack.mat file with the folowing variables :
 %   
 %   STACK_NET_<N> 4D volumes stacking networks for network N across individual maps  
-%   MEAN          4D volumes stacking networks for the mean networks across individual maps.
-%   STD           4D volumes stacking networks for the std networks across individual maps.
+%   MEAN_NET_<N>  4D volumes stacking networks for the mean networks across individual maps.
+%   STD_NET_<N>   4D volumes stacking networks for the std networks across individual maps.
 %
 % OPT  (structure, optional) with the following fields:
 %
@@ -47,15 +51,16 @@ function [files_in,files_out,opt] = niak_brick_build_stack(files_in,files_out,op
 
 %% Initialization and syntax checks
 
-% Syntax
-if ~exist('files_in','var')
-    error('niak:brick','syntax: [FILES_IN,FILES_OUT,OPT] = NIAK_BRICK_BUILD_STACK(FILES_IN,FILES_OUT,OPT).\n Type ''help niak_brick_build_stack'' for more info.')
-end
-
 % Input
 if ~isstruct(files_in)
-    error('FILES_IN should be a structure. Type "help niak_brick_build_stack" for more info.');
+    error('niak:brick','FILES_IN should be a structure.\n Type ''help niak_brick_build_stack'' for more info.');
 end
+
+if isempty(files_in.mask)
+    error('niak:brick','Mask missing.\n Type ''help niak_brick_build_stack'' for more info.')
+end
+% Read mask
+[hdr_m,mask] = niak_read_vol(files_in.mask);
 
 % Output
 if ~exist('files_out','var')||isempty(files_out)
@@ -81,22 +86,26 @@ end
 
 % setup list of networks
 if isempty(opt.scale)
-   [hdr,vol]=niak_read_vol(files_in.map.(fieldnames(files_in.map){1}))
+   [hdr,vol]=niak_read_vol(files_in.map.(fieldnames(files_in.map){1}));
    list_network =  hdr.info.dimensions(end);
    list_network = [1 : list_network];
 else
    list_network = cell2mat(opt.scale);
 end
-   
 
 % Network 4D volumes with M subjects
 for ss = list_network
-    for ii = 1:length(files_in.map)
-        sub = files_in.map{ii};
-        path_vol = [path_in sub];
-        [hdr,vol] = niak_read_vol(path_vol);
-        stack(:,:,:,ii) = vol(:,:,:,ss);
+    for ii = 1:length(fieldnames(files_in.map))
+        sub = fieldnames(files_in.map){ii};
+        fprintf('loading net_%d:\n',ss)
+        niak_progress(ii,length(fieldnames(files_in.map)),1)
+        [hdr,vol] = niak_read_vol(files_in.map.(sub));
+        tseries = niak_vol2tseries(vol,mask);
+        eval(sprintf('stack_net_%d(:,:,:,ii) = vol(:,:,:,ss);',ss));
     end
+    
+    
+    
     hdr.file_name = [path_out 'stack_net_' num2str(ss) '.nii.gz'];
     niak_write_vol(hdr,stack);
 
