3ca6daf0f0a425b27553e0a1b26f13b18efd5e85
angela-tam <angela.tam08@gmail.com>
2016-04-16 01:13:12 -0400

    drafting building contingency table

--- extensions/subtype/niak_brick_subtyping.m
+++ extensions/subtype/niak_brick_subtyping.m
@@ -10,8 +10,9 @@ function [files_in,files_out,opt] = niak_brick_subtyping(files_in,files_out,opt)
 % FILES_IN 
 %       (structure) with the following fields:
 %
-%   DATA (string) path to a .mat file containing an array (#subjects x
-%   #voxels OR vertices OR regions) generated from subtype_preprocessing
+%   DATA 
+%       (string) path to a .mat file containing an array (#subjects x
+%       #voxels OR vertices OR regions) generated from subtype_preprocessing
 %
 %   HIER
 %       (string) path to a .mat file containing a variable HIER which is a
@@ -20,6 +21,10 @@ function [files_in,files_out,opt] = niak_brick_subtyping(files_in,files_out,opt)
 %   MASK
 %       (3D volume, default all voxels) a binary mask of the voxels that 
 %       are included in the time*space array
+%
+%   MODEL
+%       (string, optional, default 'gb_niak_omitted') the name of a csv file
+%       containing information and variables about subjects 
 % 
 % FILES_OUT 
 %       (string) path for results
@@ -31,9 +36,24 @@ function [files_in,files_out,opt] = niak_brick_subtyping(files_in,files_out,opt)
 %       (integer) the number of desired subtypes
 %
 %   SUB_MAP_TYPE 
-%       (string, default 'mean') how the subtypes are represented in
+%       (string, default 'mean') how the subtypes are represented in the
 %       volumes
-%       (options: 'mean' or 'median') 
+%       (options: 'mean' or 'median')
+%
+%   NB_COL_CSV
+%       (integer, optional, default 'gb_niak_omitted') the column number
+%       (excluding column A for subject IDs) in the model csv that separates 
+%       subjects into groups to compare chi-squared and Cramer's V stats
+%
+% % % % % % % % % %   NB_GROUP
+% % % % % % % % % %       (integer, optional, default 'gb_niak_omitted') the number of
+% % % % % % % % % %       distinct groups from the column in NB_COL_CSV to compare
+% % % % % % % % % %       chi-squared and Cramer's V stats
+%
+%   FLAG_STATS
+%       (boolean, optional, default false) if the flag is true, the brick
+%       will calculate Cramer's V and chi-squared statistics for groups
+%       specified in files_in.model
 %
 %   FLAG_VERBOSE
 %       (boolean, optional, default true) turn on/off the verbose.
@@ -66,10 +86,13 @@ end
 
 % Input
 if ~isstruct(files_in)
-    error('FILES_IN should be a structure with the subfields DATA, HIER, and MASK');
+    error('FILES_IN should be a structure with the required subfields DATA, HIER, and MASK');
 end
-list_fields   = { 'data' , 'hier' , 'mask' };
-list_defaults = { NaN    , NaN    , NaN    };
+if isfield(files_in,'model') && ~ischar(files_in.model)
+    error('FILES_IN.MODEL should be a string');
+end
+list_fields   = { 'data' , 'hier' , 'mask', 'model' };
+list_defaults = { NaN    , NaN    , NaN   , 'gb_niak_omitted' }; 
 files_in = psom_struct_defaults(files_in,list_fields,list_defaults);
 
 % Output
@@ -87,8 +110,8 @@ end
 if ~isstruct(opt)
     error('OPT should be a structure where the subfield NB_SUBTYPE must be specified with an integer');
 end
-list_fields   = { 'nb_subtype', 'sub_map_type', 'flag_verbose' , 'flag_test' };
-list_defaults = { NaN         , 'mean'        , true           , false       };
+list_fields   = { 'nb_subtype', 'sub_map_type', 'nb_col_csv'     , 'nb_group'       , 'flag_stats', 'flag_verbose' , 'flag_test' };  %%%%%%%%
+list_defaults = { NaN         , 'mean'        , 'gb_niak_omitted', 'gb_niak_omitted', false       , true           , false       };
 opt = psom_struct_defaults(opt,list_fields,list_defaults);
 
 % If the test flag is true, stop here !
@@ -157,6 +180,98 @@ vol_eff_sub = niak_tseries2vol(sub.mean_eff,mask);
 file_name = 'eff_subtype.nii.gz';
 hdr.file_name = fullfile(files_out, file_name);
 niak_write_vol(hdr,vol_eff_sub);
+
+%% Calculate Cramer's V and Chi2
+
+if opt.flag_stats == 1 && ~strcmp(files_in.model,'gb_niak_omitted') && ~strcmp(opt.nb_col_csv,'gb_niak_omitted') %%%% && ~strcmp(opt.nb_group,'gb_niak_omitted')
+    [tab,sub_id,labels_y] = niak_read_csv(files_in.model);
+    
+    % build the model from user's csv and input column
+    model = [ones(length(sub_id),1) tab(:,opt.nb_col_csv)];
+    % build a mask for NaN values in model and mask out subjects with NaNs
+    mask_nan = ~max(isnan(model),[],2);
+    model = model(mask_nan,:);
+    grp = model(:,2);
+    sub_id = sub_id(mask_nan,:);
+    part = part(mask_nan,:);
+        
+    % build the contingency table 
+    
+    list_gg = unique(model(:,2))';
+    for cc = 1:opt.nb_subtype
+        for gg = 1:length(list_gg) % for each group
+            mask_sub = find(part(:)==cc); % build a mask to select subjects within one cluster 
+            sub_model = grp(mask_sub); % subjects within one cluster
+            nn = numel(find(sub_model(:)==list_gg(gg))); % number of subjects for a single group that is in the cluster
+            contab(gg,cc) = nn;
+        end
+    end
+ 
+        
+ 
+% %%% pierre orban's example   
+%     for cc = 1:nb_clus
+%         mask_hc = tab(:,num_admci)==min(tab(:,num_admci));
+%         part_hc = part(mask_hc);
+%         p=0;
+%         for pp = 1:length(part_hc)
+%             if part_hc(pp) == cc
+%                 p=p+1;
+%             end
+%         end
+%         ct(1,cc) = p;
+%         
+%         mask_admci = tab(:,num_admci)==max(tab(:,num_admci));
+%         part_admci = part(mask_admci);
+%         p=0;
+%         for pp = 1:length(part_admci)
+%             if part_admci(pp) == cc
+%                 p=p+1;
+%             end
+%         end
+%         ct(2,cc) = p;
+%     end
+%     
+%     [~,pchi,~] = chi2cont(ct);
+%     statschi(n_net,1) = pchi;
+%     
+%     opt.labels_x = name_grp;
+%     opt.labels_y = name_clus;
+%     opt.precision = 2;
+%     path_ct = [path_res_net 'chi2_ct.csv'];
+%     niak_write_csv(path_ct,ct,opt)
+%     
+%     stats=mestab(ct);
+%     Vchi(n_net,1) = stats.cramerV;
+    
+    % calculate chi-squared and cramer's v
+    
+    % 
+% function [es,esCi,chi2]=cramerv(table,nRow,nCol,confLevel,is2by2)
+% % this function computes Cramer's V, including exact analytical CI
+% % ** NOTE: in the case of 2 by 2 tables Cramer's V is identical to phi
+% % except possibly for the sign), which will be taken care of in the last
+% % lines
+
+%     colSum=sum(table);
+%     rowSum=sum(table,2);
+%     n=sum(sum(table));
+%     k=min(nRow,nCol);
+%     df=(nRow-1)*(nCol-1);
+%     % expected frequency of occurrence in each cell: product of row and
+%     % column totals divided by total N
+%     ef=(rowSum*colSum)/n;
+%     % chi square stats
+%     chi2=(table-ef).^2./ef;
+%     chi2=sum(chi2(:));
+%     % Cramer's V
+%     es=sqrt(chi2/(n*(k-1)));
+
+
+% save the stats into a structure variable 'stats' with subfields
+ 
+
+end
 
 %% Saving subtyping results and statistics
 
