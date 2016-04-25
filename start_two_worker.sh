1f152930cb316bf269e05e5d7c632e080229b85c
Pierre-Olivier Quirion <pioliqui@gmail.com>
2016-04-25 11:43:19 -0400

    Add adhoc script to start manualy psom agents

---
+++ start_two_worker.sh
@@ -0,0 +1,8 @@
+~/simexp/psom/psom_worker.py -d /home/poquirion/test/result -w manager &
+~/simexp/psom/psom_worker.py -d /home/poquirion/test/result -w garbage &
+~/simexp/psom/psom_worker.py -d /home/poquirion/test/result -w 1 &
+~/simexp/psom/psom_worker.py -d /home/poquirion/test/result -w 2 &
+~/simexp/psom/psom_worker.py -d /home/poquirion/test/result -w 3 &
+~/simexp/psom/psom_worker.py -d /home/poquirion/test/result -w 4 &
+~/simexp/psom/psom_worker.py -d /home/poquirion/test/result -w 5 &
+~/simexp/psom/psom_worker.py -d /home/poquirion/test/result -w 6 &
