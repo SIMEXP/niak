067942774d8b46e15ce8c69ed50a60a2fc2538de
surchs <sebastian.urchs@gmail.com>
2016-04-26 16:29:03 -0400

    some path changes

--- extensions/subtype/test_similarity_matrix.m
+++ extensions/subtype/test_similarity_matrix.m
@@ -2,13 +2,14 @@
 clear;
 
 %% Set up paths
-stack_path = '/home/surchs/GDrive/PhD/TeamStuff/Niak_Stuff/subtype_test_data/out/seb/stack_file.mat';
-out_path = '/home/surchs/GDrive/PhD/TeamStuff/Niak_Stuff/subtype_test_data/out/seb/';
+base_path = '/home/surchs/GDrive/PhD/TeamStuff/Niak_Stuff/';
+stack_path = [base_path 'subtype_test_data/out/seb/stack_file.mat'];
+out_path = [base_path '/subtype_test_data/out/seb/'];
 
 %% Set up inputs
 files_in = stack_path;
 files_out = out_path;
-opt.flag_test = true;
+opt.flag_test = false;
 
 %% Call the thing
 niak_brick_similarity_matrix(files_in,files_out,opt);
