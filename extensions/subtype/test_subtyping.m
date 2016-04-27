d04d635882dc3af495e275f5942983e7f959d022
surchs <sebastian.urchs@gmail.com>
2016-04-27 16:37:48 -0400

    some path edits

--- extensions/subtype/test_subtyping.m
+++ extensions/subtype/test_subtyping.m
@@ -2,11 +2,11 @@
 clear;
 
 %% Set up the paths
-base_path = '/home/surchs/GDrive/PhD/TeamStuff/Niak_Stuff/';
-sim_path = [base_path '/subtype_test_data/out/seb/similarity_matrix.mat'];
-mask_path = [base_path '/subtype_test_data/raw/func_mask_group_stereonl.mnc.gz'];
-model_path = [base_path '/subtype_test_data/pheno/numeric_pheno.csv'];
-out_path = [base_path '/subtype_test_data/out/seb/'];
+base_path = '/home/surchs/GDrive/PhD/TeamStuff/Niak_Stuff/subtype_test_data/';
+sim_path = [base_path 'out/seb/similarity_matrix.mat'];
+mask_path = [base_path 'raw/func_mask_group_stereonl.mnc.gz'];
+model_path = [base_path 'pheno/numeric_pheno.csv'];
+out_path = [base_path 'out/seb/'];
 
 %% Set up inputs
 files_in = struct;
@@ -17,6 +17,7 @@ files_in.model = model_path;
 files_out = out_path;
 
 opt = struct;
+opt.nb_subtype = 3;
 
 %% Call the brick
 niak_brick_subtyping(files_in,files_out,opt)
