3a223233202548d95c9d52f83c0277ee7a9ccda7
Pierre Bellec <pierre.bellec@gmail.com>
2016-04-30 13:11:45 -0400

    Simplified code and doc for the nifti reader. WIP.

--- commands/read_write/niak_read_nifti.m
+++ commands/read_write/niak_read_nifti.m
@@ -4,57 +4,37 @@ function [hdr,vol] = niak_read_nifti(file_name)
 % affine transformation information will be included.
 % http://nifti.nimh.nih.gov/nifti-1
 %
-% SYNTAX:
-% [HDR,VOL] = NIAK_READ_NIFTI(FILE_NAME)
+% SYNTAX: [HDR,VOL] = NIAK_READ_NIFTI(FILE_NAME)
 %
-% _________________________________________________________________________
-% INPUT:
-%
-% FILE_NAME         
-%    (string) a 3D+t or 3D minc file.
-%
-% _________________________________________________________________________
-% OUTPUT:
-%
-% VOL           
-%    (3D+t or 3D array of double) the fMRI or MRI data.
-%
-% HDR           
-%    a structure containing a description of the data. See NIAK_READ_VOL 
+% FILE_NAME (string) a 3D+t or 3D minc nifti file.
+% VOL (3D+t or 3D array of double) the fMRI or MRI data.
+% HDR (structure) a description of the data. See NIAK_READ_VOL 
 %    and NIAK_READ_HDR_NIFTI for details.
 %
-% _________________________________________________________________________
-% SEE ALSO:
+% SEE ALSO: NIAK_READ_HDR_NIFTI, NIAK_WRITE_NIFTI, NIAK_READ_VOL, 
+%     NIAK_WRITE_VOL, NIAK_READ_DATA_NIFI
 %
-% NIAK_READ_HDR_NIFTI, NIAK_WRITE_NIFTI, NIAK_READ_VOL, NIAK_WRITE_VOL
-%
-% _________________________________________________________________________
 % COMMENTS:
+%   In case of multiple files data (e.g. .IMG + .HDR, or .IMG + .HDR + .MAT),
+%   specify the name using the .IMG extension only.
 %
-% In case of multiple files data (e.g. .IMG + .HDR, or .IMG + .HDR + .MAT),
-% specify the name using the .IMG extension only.
-%
-% The affine transformation in hdr.info corresponds to the sform, if specified, or 
-% the qform (if sform unspecified and qform is), or simply the pixel dimension 
-% and offset, if neither qform or sform are specified. 
+%   The affine transformation in hdr.info corresponds to the sform, if specified, or 
+%   the qform (if sform unspecified and qform is), or simply the pixel dimension 
+%   and offset, if neither qform or sform are specified. 
 % 
-% Part of this file is copied and modified under GNU license from
-% MRI_TOOLBOX developed by CNSP in Flinders University, Australia
+%   The data is forced to single precision.
 %
-% Important parts of this code are copied and modified from a matlab
-% toolbox by Jimmy Shen (pls@rotman-baycrest.on.ca) who agreed for 
-% redistribution under the MIT license. 
-%
-% Note that the precision of the data is forced to single (32 bits). 
-% 
-% Copyright (c) Pierre Bellec, 
+% See license in the code. 
+ 
+% Copyright (c) Pierre Bellec, Jimmy Shen, 2008-2016.
 % Montreal Neurological Institute, 2008-2010
-% Departement d'informatique et de recherche operationnelle
-% Centre de recherche de l'institut de Geriatrie de Montreal
-% Universite de Montreal, 2011-2015
+% Centre de recherche de l'institut de geriatrie de Montreal, 
+% Department of Computer Science and Operations Research
+% University of Montreal, Qubec, Canada, 2010-2016
+% Maintainer: pierre.bellec@criugm.qc.ca
 % See licensing information in the code.
-% Keywords : medical imaging, I/O, reader, nifti
-
+% Keywords: medical imaging, I/O, reader, nifti
+%
 % Permission is hereby granted, free of charge, to any person obtaining a copy
 % of this software and associated documentation files (the "Software"), to deal
 % in the Software without restriction, including without limitation the rights
@@ -73,16 +53,12 @@ function [hdr,vol] = niak_read_nifti(file_name)
 % OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 % THE SOFTWARE.
 
-if nargin < 2
-    precision_data = 'float';
-end
-
 [path_f,name_f,ext_f] = fileparts(file_name);
 if isempty(path_f)
     path_f = '.';
 end
 switch ext_f
-    case '.nii'
+    case {'.nii','.nii.gz'}
         file_header = file_name;
     case '.img'
         file_header = cat(2,path_f,filesep,name_f,'.hdr');
@@ -92,83 +68,7 @@ end
 
 %% Parsing the header
 hdr = niak_read_hdr_nifti(file_header);
-
+hdr.info.file_parent = file_name;
 if nargout > 1
-    %% Opening the data file
-
-    fid = fopen(file_name,'r',hdr.info.machine);
-
-    if fid < 0,
-        msg = sprintf('Cannot open file %s.',file_name);
-        error(msg);
-    end
-
-    if hdr.details.dim(5) < 1
-        hdr.details.dim(5) = 1;
-    end
-
-    %  move pointer to the start of image block
-    %
-    switch hdr.type
-        case {'vol', 'analyze'}
-            fseek(fid, 0, 'bof');
-        case 'nii'
-            fseek(fid, hdr.details.vox_offset, 'bof');
-    end
-
-
-    %  For each frame, precision of value will be read
-    %  in vol_siz times, where vol_siz is only the
-    %  dimension size of an image, not the byte storage
-    %  size of an image.
-    vol_siz = prod(hdr.details.dim(2:5));
-
-    %%  For complex float32 or complex float64, voxel values
-    %%  include [real, imag]
-    if (hdr.details.datatype == 32) || (hdr.details.datatype == 1792)
-        vol_siz = vol_siz * 2;
-    end
-
-    %% MPH: For RGB24, voxel values include 3 separate color planes
-    if (hdr.details.datatype == 128) || (hdr.details.datatype == 511)
-        vol_siz = vol_siz * 3;
-    end
-
-    vol = fread(fid, vol_siz, sprintf('*%s',hdr.info.precision));
-
-    %%  For complex float32 or complex float64, voxel values
-    %%  include [real, imag]
-    if (hdr.details.datatype == 32) || (hdr.details.datatype == 1792)
-        vol = reshape(vol, [2, length(vol)/2]);
-        vol = complex(vol(1,:)', vol(2,:)');
-    end
-
-    fclose(fid);
-
-    %%  Update the global min and max values
-    hdr.details.glmax = max(double(vol(:)));
-    hdr.details.glmin = min(double(vol(:)));
-
-    %% Reshape the volume to correct dimensions
-    vol_idx = 1:hdr.details.dim(5);
-
-    if (hdr.details.datatype == 128) && (hdr.details.bitpix == 24)
-        vol = squeeze(reshape(vol, [3 hdr.details.dim(2:4) length(vol_idx)]));
-        vol = permute(vol, [2 3 4 1 5]);
-    elseif (hdr.details.datatype == 511) && (hdr.details.bitpix == 96)
-        vol = single(vol);
-        vol = (vol - min(vol))/(max(vol) - min(vol));
-        vol = squeeze(reshape(vol, [3 hdr.details.dim(2:4) length(vol_idx)]));
-        vol = permute(vol, [2 3 4 1 5]);
-    else
-        vol = squeeze(reshape(vol, [hdr.details.dim(2:4) length(vol_idx)]));
-    end
-    
-    if ((hdr.details.scl_slope~=0)&&(hdr.details.scl_slope~=1))||(hdr.details.scl_inter~=0)
-        vol = hdr.details.scl_slope * single(vol) + hdr.details.scl_inter;
-        hdr.details.scl_slope = 1;
-        hdr.details.scl_inter = 0;
-    end  
-    vol = single(vol);
-    hdr.info.precision = 'float';
+    vol = niak_read_data_nifti(hdr);
 end
