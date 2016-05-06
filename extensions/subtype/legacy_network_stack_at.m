f5f7480d4cdd53ecc05e89db3af55bb782e4b72f
angela-tam <angela.tam08@gmail.com>
2016-05-06 15:35:47 -0400

    added confound regression

--- extensions/subtype/legacy_network_stack_at.m
+++ extensions/subtype/legacy_network_stack_at.m
@@ -1,15 +1,14 @@
-%% script to build stack maps with prevent-ad data release 2.0
-% 231 subjects with resting-state (concatenated rest 1 and rest 2)
+%% script to build stack maps with hcp 20 subject dataset
 
 clear all
 
 %% set paths
 
-path_in = '/Users/AngelaTam/Desktop/adsf/scores/rmap_part_20160401_s007_avg/';
-path_out = '/Users/AngelaTam/Desktop/subtype_pipeline_test/preventad_test/legacy/';
-path_model = '/Users/AngelaTam/Desktop/adsf/model/preventad_model_20160408.csv';
+path_in = '/Users/AngelaTam/Desktop/subtype_pipeline_test/subtype_test_data/raw_nii/';
+path_out = '/Users/AngelaTam/Desktop/subtype_pipeline_test/subtype_test_data/legacy/';
+path_model = '/Users/AngelaTam/Desktop/subtype_pipeline_test/subtype_test_data/niak_pheno.csv';
 mask_path = '/Users/AngelaTam/Desktop/subtype_pipeline_test/subtype_test_data/func_mask_group_stereonl.nii.gz';
-scale = 7;
+n_net = 7; % number of networks
 
 %% Read model
 [tab,id,~,~] = niak_read_csv(path_model);
@@ -18,10 +17,10 @@ scale = 7;
 [hdr_mask,mask] = niak_read_vol(mask_path);
 
 %% Network 4D volumes with M subjects
-for ss = 1:scale
+for ss = 1:n_net
     for ii = 1:length(id)
         sub = id{ii};
-        path_vol = [path_in sub '*.nii.gz'];
+        path_vol = [path_in 'HCP' sub '*.nii.gz'];
         [hdr,vol] = niak_read_vol(path_vol);
         stack(:,:,:,ii) = vol(:,:,:,ss);
     end
@@ -29,10 +28,36 @@ for ss = 1:scale
 %     hdr.file_name = [path_out 'stack_net_' num2str(ss) '.nii.gz'];
 %     niak_write_vol(hdr,stack);
     
-    %% Transform 4D stack maps to subject x voxel array
-    stack_net = ['stack_net_' num2str(ss)];
-    stack_net = niak_vol2tseries(stack,mask);
+    %% Transform the raw 4D stack maps to subject x voxel array
+    raw_stack = niak_vol2tseries(stack,mask);
+    
+    %% regress out confounding variables
+    
+    model.x = [ones(length(id),1) tab(:,1) tab(:,2)]; % set up model for glm
+    mask_nnan = ~max(isnan(model.x),[],2);
+    model.x = model.x(mask_nnan,:);
+    stack_net = raw_stack(mask_nnan,:,:);  % putting a mask to get rid of NaNs over the newly created variable raw_stack
+    tab = tab(mask_nnan,:); % mask to get rid of NaNs within tab
+    id = id(mask_nnan);
+    
+    for nn = 1:size(stack_net,3)
+        model.y = stack_net(:,:,nn);
+        model.c = [1 0 0];
+        %     opt_glm.test = 'ttest';
+        opt_glm.flag_residuals = true;
+        glm = niak_glm(model,opt_glm);
+        stack_net(:,:,nn) = glm.e;
+    end
+    
+    %% Save the mat file
     mat_name = [path_out 'stack_net_' num2str(ss) '.mat'];
     save(mat_name,'stack_net')
 
 end
+
+
+
+
+
+
+
