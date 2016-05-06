fdb3913429fec17d362d6bb65db63fb963f65c81
angela-tam <angela.tam08@gmail.com>
2016-05-06 15:36:09 -0400

    changed to hcp dataset

--- extensions/subtype/test_network_stack_at.m
+++ extensions/subtype/test_network_stack_at.m
@@ -1,14 +1,14 @@
-%% test niak_brick_network_stack with preventad dataset
+%% test niak_brick_network_stack with 20 subj hcp dataset
 
 %% Wipe
 clear;
 
 %% Set up the paths
-data_base = '/Users/AngelaTam/Desktop/adsf/';
-model = [data_base 'model/preventad_model_20160408.csv'];
-data_path = [data_base 'scores/rmap_part_20160401_s007_avg/'];
-mask_path = '/Users/AngelaTam/Desktop/subtype_pipeline_test/subtype_test_data/func_mask_group_stereonl.nii.gz';
-out_path = '/Users/AngelaTam/Desktop/subtype_pipeline_test/preventad_test/';
+data_base = '/Users/AngelaTam/Desktop/subtype_pipeline_test/subtype_test_data/';
+model = [data_base 'niak_pheno.csv'];
+data_path = [data_base 'raw_nii/'];
+mask_path = [data_base 'func_mask_group_stereonl.nii.gz'];
+out_path = [data_base '/pipeline/'];
 
 %% Get the model and load the files
 [tab, ~, cat_names, ~] = niak_read_csv(model);
@@ -27,9 +27,9 @@ end
 
 %% Set up inputs
 for ind = 2:size(pheno,1)
-    sub_name = pheno{ind, go_ind};
+    sub_name = ['HCP' pheno{ind, go_ind}];
     % Get the file name and path
-    file_name = sprintf('%s_BL00_avg_rmap_part.nii.gz', sub_name);
+    file_name = sprintf('%s_sess1_motLR_stability_maps.nii.gz', sub_name);
     file_path = [data_path filesep file_name];
     files_in.data.(sub_name) = file_path;
 end
@@ -38,9 +38,9 @@ files_in.mask = mask_path;
 files_in.model = model;
 files_out = '';
 
-opt.network = 7;
+opt.network = 3;
 opt.folder_out = out_path;
-opt.regress_conf = {'gender', 'age'};
+opt.regress_conf = {'Gender', 'Age'};
 opt.flag_conf = true;
 opt.flag_test = false;
 
