da2171cf864ab52e9f47ba7ea471df6a351db8a9
surchs <sebastian.urchs@gmail.com>
2016-05-09 13:10:48 -0400

    association test

---
+++ extensions/subtype/test_association.m
@@ -0,0 +1,23 @@
+%% Wipe
+clear;
+
+%% Set up the paths
+base_path = '/home/surchs/GDrive/PhD/TeamStuff/Niak_Stuff/subtype_test_data/';
+model_path = [base_path '/pheno/numeric_pheno.csv'];
+weight_path = [base_path '/out/seb/subtype_weights.mat'];
+out_path = [base_path 'out/seb/'];
+
+%% Set up inputs
+files_in = struct;
+files_in.weight = weight_path;
+files_in.model = model_path;
+files_out = out_path;
+
+opt.network = 3;
+opt.folder_out = out_path;
+opt.regress_conf = {'Gender', 'Age'};
+opt.flag_conf = true;
+opt.flag_test = false;
+
+%% Run the brick
+[files_in, files_out, opt] = niak_brick_association_test(files_in, files_out, opt);
