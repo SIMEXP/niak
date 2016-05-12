8f772b87ef4fc1bd16677c5d55d9f0a6da2dfb3e
angela-tam <angela.tam08@gmail.com>
2016-05-12 14:12:32 -0400

    paths to hcp dataset

--- extensions/subtype/test_similarity_matrix_at.m
+++ extensions/subtype/test_similarity_matrix_at.m
@@ -4,9 +4,9 @@ clear all
 
 % set up paths
 
-files_in = '/Users/AngelaTam/Desktop/subtype_pipeline_test/preventad_test/network_stack.mat';
+files_in = '/Users/AngelaTam/Desktop/subtype_pipeline_test/subtype_test_data/pipeline/network_stack.mat';
 files_out = struct;
-opt.folder_out = '/Users/AngelaTam/Desktop/subtype_pipeline_test/preventad_test/';
+opt.folder_out = '/Users/AngelaTam/Desktop/subtype_pipeline_test/pipeline/';
 
 % call the brick
 niak_brick_similarity_matrix(files_in,files_out,opt);
