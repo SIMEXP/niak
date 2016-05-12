5f1789fcbd07be224536945d032a5f8be9340ed9
angela-tam <angela.tam08@gmail.com>
2016-05-12 14:14:15 -0400

    comments

--- extensions/subtype/test_similarity_matrix_at.m
+++ extensions/subtype/test_similarity_matrix_at.m
@@ -1,4 +1,4 @@
-%% test script for niak_brick_similarity_matrix with preventad dataset
+%% test script for niak_brick_similarity_matrix with hcp dataset
 
 clear all
 
@@ -6,7 +6,7 @@ clear all
 
 files_in = '/Users/AngelaTam/Desktop/subtype_pipeline_test/subtype_test_data/pipeline/network_stack.mat';
 files_out = struct;
-opt.folder_out = '/Users/AngelaTam/Desktop/subtype_pipeline_test/pipeline/';
+opt.folder_out = '/Users/AngelaTam/Desktop/subtype_pipeline_test/subtype_test_data/pipeline';
 
 % call the brick
 niak_brick_similarity_matrix(files_in,files_out,opt);
