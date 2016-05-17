5ca90e3d12b185efc392bdf9aabe1ba80e696f78
surchs <sebastian.urchs@gmail.com>
2016-05-17 16:54:19 -0400

    Result output implemented
    Figures, csv and stats saved to specified files

--- extensions/subtype/niak_brick_association_test.m
+++ extensions/subtype/niak_brick_association_test.m
@@ -20,10 +20,18 @@ function [files_in,files_out,opt] = niak_brick_association_test(files_in, files_
 %       have a header and a first column specifying the case IDs/names
 %       corresponding to the data in FILES_IN.DATA
 %
-%
 % FILES_OUT
-%   (string) absolute path to the output .mat file containing the
-%   association results.
+%   (structure) with the following fields:
+%
+%   STATS
+%       (string) path to the .mat file containing the fitted GLM and FDR results
+%
+%   CSV
+%       (string, default 'results_overview.csv') path to the .csv file 
+%       containing the overview of significant results passing FDR
+%
+%   FIGURES
+%       (cell array,  default 'fig_association_<NETWORK>.pdf') path to the ...
 %
 % OPT
 %   (structure, optional) with the following fields:
@@ -200,11 +208,12 @@ end
 files_in = psom_struct_defaults(files_in,...
            { 'weight' , 'model' },...
            { NaN      , NaN     });
-
-% FILES_OUT
-if ~ischar(files_out)
-    error('FILES_OUT should be a string');
-end
+       
+% Need to read the weights for default values
+tmp = load(files_in.weight);
+weights = tmp.weight_mat;
+% Figure out how many cases we are dealing with
+[n_sub, n_sbt, n_net] = size(weights);
 
 % Options
 if nargin < 3
@@ -212,9 +221,21 @@ if nargin < 3
 end
 
 opt = psom_struct_defaults(opt,...
-      { 'folder_out' , 'network' , 'test_name' , 'fdr' , 'type_fdr' , 'contrast' , 'interaction' , 'normalize_x' , 'normalize_y' , 'normalize_type' , 'select' , 'flag_intercept' , 'flag_filter_nan' , 'flag_verbose' , 'flag_test' },...
+      { 'folder_out' , 'network'  'test_name' , 'fdr' , 'type_fdr' , 'contrast' , 'interaction' , 'normalize_x' , 'normalize_y' , 'normalize_type' , 'select' , 'flag_intercept' , 'flag_filter_nan' , 'flag_verbose' , 'flag_test' },...
       { ''           , []        , NaN         , 0.05  , 'BH'       , struct     , struct        , true          , false         ,  'mean'          , struct   , true             , true              , true           , false       });
 
+% FILES_OUT
+if ~isempty(opt.folder_out)
+    path_out = niak_full_path(opt.folder_out);
+    files_out = psom_struct_defaults(files_out,...
+                { 'stats'                            , 'csv'                             , 'figures'                                                     },...
+                { [path_out 'association_stats.mat'] , [path_out 'results_overview.csv'] , make_paths(path_out, 'fig_association_net_%d.png', 1:n_net) });
+else
+    files_out = psom_struct_defaults(files_out,...
+                { 'stats'           , 'csv'             , 'figures'         },...
+                { 'gb_niak_omitted' , 'gb_niak_omitted' , 'gb_niak_omitted' });
+end
+  
 %% Sanity Checks
 % Since we don't know which optional parameters were set, we'll remove the
 % empty default values again so they don't cause trouble downstream
@@ -248,6 +269,13 @@ if ~isstruct(opt.contrast)
     error('OPT.CONTRAST has to be a structure');
 end
 
+% Check that we have an output name for every figure
+if ~strcmp(files_out.figures, 'gb_niak_omitted')
+    if ~length(files_out.figures) == n_net
+        error('I have %d networks from FILES_IN.WEIGHTS but %d specified output files for FILES_OUT.FIGURES', n_nets, length(files_out.figures));
+    end
+end
+
 %% If the test flag is true, stop here !
 if opt.flag_test == 1
     return
@@ -264,17 +292,6 @@ end
 model_raw.x = model_data;
 model_raw.labels_x = labels_x;
 model_raw.labels_y = labels_y;
-
-% Read the weight data
-if opt.flag_verbose
-    fprintf('Reading the weight data ...\n');
-end
-
-% Read the weights file
-tmp = load(files_in.weight);
-weights = tmp.weight_mat;
-% Figure out how many cases we are dealing with
-[n_sub, n_sbt, n_net] = size(weights);
 
 % Prepare the variable for the p-value storage
 pvals = zeros(n_net, n_sbt);
@@ -312,116 +329,158 @@ end
 [fdr,fdr_test] = niak_fdr(pvals, opt.type_fdr, opt.fdr);
 
 % Save the model and FDR test
-%save(files_out, 'fdr', 'fdr_test', 'results');
+if ~strcmp(files_out.stats, 'gb_niak_omitted')
+    if opt.flag_verbose
+        fprintf('Saving stats to %s\n', files_out.stats);
+    end
+    save(files_out.stats, 'fdr', 'fdr_test', 'results');
+end
 
 %% Create result summaries
-[net_ids, sbt_ids] = find(fdr_test);
-% Sort the subtypes by the network IDs
-[~, ind] = sort(net_ids);
-net_ids = net_ids(ind);
-sbt_ids = sbt_ids(ind);
-% Check if any results passed FDR
-if isempty(net_ids)
-    warning('No results passed FDR');
-    out_str = 'No results passed FDR';
-else
-    out_str = 'Network,Subtype,Association,T_value,P_value,FDR\n';
-    % Iterate over the significant findings
-    for res_id = 1:length(net_ids)
-        net_id = net_ids(res_id);
-        sbt_id = sbt_ids(res_id);
-        net_name = net_names{net_id};
-        % Get the corresponding T-, p-, and FDR-values
-        t_val = glm_results.(net_name).ttest(sbt_id);
-        p_val = pvals(net_id, sbt_id);
-        fdr_val = fdr(net_id, sbt_id);
-        % Determine the direction of the association
-        if t_val > 0
-            direction = 'positive';
-        else
-            direction = 'negative';
+if ~strcmp(files_out.csv, 'gb_niak_omitted')
+    [net_ids, sbt_ids] = find(fdr_test);
+    % Sort the subtypes by the network IDs
+    [~, ind] = sort(net_ids);
+    net_ids = net_ids(ind);
+    sbt_ids = sbt_ids(ind);
+    % Check if any results passed FDR
+    if isempty(net_ids)
+        warning('No results passed FDR');
+        out_str = 'No results passed FDR';
+    else
+        out_str = 'Network,Subtype,Association,T_value,P_value,FDR\n';
+        % Iterate over the significant findings
+        for res_id = 1:length(net_ids)
+            net_id = net_ids(res_id);
+            sbt_id = sbt_ids(res_id);
+            net_name = net_names{net_id};
+            % Get the corresponding T-, p-, and FDR-values
+            t_val = glm_results.(net_name).ttest(sbt_id);
+            p_val = pvals(net_id, sbt_id);
+            fdr_val = fdr(net_id, sbt_id);
+            % Determine the direction of the association
+            if t_val > 0
+                direction = 'positive';
+            else
+                direction = 'negative';
+            end
+            % Assemble the out string
+            out_str = [out_str sprintf('%s,%d,%s,%d,%d,%d\n', net_name, sbt_id,...
+                                                             direction, t_val,...
+                                                             p_val,fdr_val)];
         end
-        % Assemble the out string
-        out_str = [out_str sprintf('%s,%d,%s,%d,%d,%d\n', net_name, sbt_id,...
-                                                         direction, t_val,...
-                                                         p_val,fdr_val)];
     end
-end
 
-% Save the string to file
-% fid = fopen(files_out,'wt');
-% fprintf(fid, out_str);
-% fclose(fid);
+    % Save the string to file
+    if opt.flag_verbose
+        fprintf('Saving results overview to %s\n', files_out.csv);
+    end
+    fid = fopen(files_out.csv,'wt');
+    fprintf(fid, out_str);
+    fclose(fid);
+end
 
 %% Visualize the weights and covariate of interest
-% First, determine if the coi is continuous or categorical - currently only
-% works if there is not more than one covariate of interest
-coi_name = model_norm.labels_y{logical(model_norm.c)};
-coi = model_norm.x(:, logical(model_norm.c));
-% Get the number unique elements of the coi
-coi_unique = unique(coi);
-n_coi_unique = length(coi_unique);
-% If there are fewer than 3 unique values, this is categorical
-if n_coi_unique < 3
-    % This is a categorical variable
-    coi_cat = true;
-    % Make an index of the coi
-    coi_ind = coi==coi_unique(1);
-else
-    % THis is a dimensional variable
-    coi_cat = false;
-end
-
-% Determine the number of rows and columns for the subyptes
-n_cols = floor(sqrt(n_sbt));
-n_rows = ceil(n_sbt/n_cols);
-
-% Make a figure for each network
-for net_id = 1:n_net
-    % Start with the figure
-    fh = figure;
-    % Go through the subtypes
-    for sbt_id = 1:n_sbt
-        % Get the subtype weights
-        sbt_weights = weights(:, sbt_id, net_id);
-        % Create the subplot
-        subplot(n_rows, n_cols, sbt_id);
-        ax = gca;
-        % Chose whether to plot categorical or dimensional data
-        if coi_cat
-            % Work around the incompatibilities between Matlab and Octave for
-            % the boxplot
-            is_octave = logical(exist('OCTAVE_VERSION', 'builtin') ~= 0);
-            % Generate the boxplots
-            if is_octave
-                % The groups are supposed to go in a cell
-                boxplot({sbt_weights(coi_ind), sbt_weights(~coi_ind)}); 
-            else
-                % The groups can be in a vector
-                boxplot(sbt_weights, coi_ind);
-            end
-            % Set the x axis ticks and labels
-            ax.XTickLabel = cellstr(num2str(coi_unique));
-        else
-            % This is a dimensional variable
-            % Fit a regression line between the weights and the covariate
-            plot_model.x = [ones(size(coi)), coi];
-            plot_model.y = sbt_weights;
-            [res, ~] = niak_glm(plot_model, struct('flag_beta', true));
-            x_fit = linspace(min(coi),max(coi),10);
-            y_fit = res.beta(1) + x_fit.*res.beta(2);
-            % Make the scatterplot
-            hold on;
-            plot(coi, sbt_weights, '.k');
-            plot(x_fit, y_fit, 'r');
-            hold off;
-            disp('done');
+if ~strcmp(files_out.figures, 'gb_niak_omitted')
+    % First, get the covariate of interest. 
+    contrasts = fieldnames(opt.contrast);
+    n_contrasts = length(contrasts);
+    coi_name = '';
+    for con_id = 1:n_contrasts
+        contrast_name = contrasts{con_id};
+        if opt.contrast.(contrast_name) ~= 0 && isempty(coi_name)
+            coi_name = contrast_name;
+        elseif opt.contrast.(contrast_name) ~= 0 && ~isempty(coi_name)
+            error('Contrast %s is nonzero but contrast %s is also nonzero. I can only deal with one contrast', contrast_name, coi_name);
         end
     end
+    % Determine whether the coi is an interaction
+    if ismember(coi_name, model_raw.labels_y)
+        % This was specified in the original model, we can take it from there. Find
+        % the index of the column
+        coi_col = find(strcmp(coi_name, model_raw.labels_y));
+        coi = model_raw.x(:, coi_col);
+    else
+        % This was not in the original model, we need to retrieve it from the
+        % normalized, contstrained model
+        coi_col = find(strcmp(coi_name, model_norm.labels_y));
+        coi = model_norm.x(:, coi_col);
+    end
+    % Determine whether the coi is categorical
+    coi_unique = unique(coi);
+    n_unique = length(coi_unique);
+    if n_unique > 2
+        coi_cat = false;
+    else
+        coi_cat = true;
+        coi_ind = coi==coi_unique(1);
+    end
+
+    % Determine the number of rows and columns for the subyptes
+    n_cols = floor(sqrt(n_sbt));
+    n_rows = ceil(n_sbt/n_cols);
+
+    % Make a figure for each network
+    for net_id = 1:n_net
+        % Start with the figure
+        fh = figure('Visible', 'off');
+        title(sprintf('Network %d, Association w %s', net_id, coi_name));
+        % Go through the subtypes
+        for sbt_id = 1:n_sbt
+            % Get the subtype weights
+            sbt_weights = weights(:, sbt_id, net_id);
+            % Create the subplot
+            subplot(n_rows, n_cols, sbt_id);
+            ax = gca;
+            % Chose whether to plot categorical or dimensional data
+            if coi_cat
+                % Work around the incompatibilities between Matlab and Octave for
+                % the boxplot
+                is_octave = logical(exist('OCTAVE_VERSION', 'builtin') ~= 0);
+                % Generate the boxplots
+                if is_octave
+                    % The groups are supposed to go in a cell
+                    boxplot({sbt_weights(coi_ind), sbt_weights(~coi_ind)}); 
+                else
+                    % The groups can be in a vector
+                    boxplot(sbt_weights, coi_ind);
+                end
+                % Set the x axis ticks and labels
+                ax.XTickLabel = cellstr(num2str(coi_unique));
+            else
+                % This is a dimensional variable
+                % Fit a regression line between the weights and the covariate
+                plot_model.x = [ones(size(coi)), coi];
+                plot_model.y = sbt_weights;
+                [res, ~] = niak_glm(plot_model, struct('flag_beta', true));
+                x_fit = linspace(min(coi),max(coi),10);
+                y_fit = res.beta(1) + x_fit.*res.beta(2);
+                % Make the scatterplot
+                hold on;
+                plot(coi, sbt_weights, '.k');
+                plot(x_fit, y_fit, 'r');
+                hold off;
+                disp('done');
+            end
+            xlabel(ax, coi_name);
+            title(sprintf('Subtype %d', sbt_id));
+        end
+        if opt.flag_verbose
+            fprintf('Saving association plot to %s\n', files_out.figures{net_id});
+        end
+        print(fh, files_out.figures{net_id}, '-dpng');
+    end
 end
-    
+end
 
-
-
-
+function path_array = make_paths(out_path, template, scales)
+    % Get the number of networks
+    n_networks = length(scales);
+    path_array = cell(n_networks, 1);
+    for sc_id = 1:n_networks
+        sc = scales(sc_id);
+        path = fullfile(out_path, sprintf(template, sc));
+        path_array{sc_id, 1} = path;
+    end
+return
 end
