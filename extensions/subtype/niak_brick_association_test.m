033318c67bd00eae463ded09c1cad3d4e07abef5
surchs <sebastian.urchs@gmail.com>
2016-06-19 17:40:30 -0400

    Full outputs saved
    Old version was wrong

--- extensions/subtype/niak_brick_association_test.m
+++ extensions/subtype/niak_brick_association_test.m
@@ -299,7 +299,7 @@ if ~strcmp(files_out.stats, 'gb_niak_omitted')
     if opt.flag_verbose
         fprintf('Saving stats to %s\n', files_out.stats);
     end
-    save(files_out.stats, 'model_norm', 'model_raw', 'fdr', 'fdr_test', 'results');
+    save(files_out.stats, 'model_norm', 'model_raw', 'fdr', 'fdr_test', 'glm_results');
 end
 
 %% Create result summaries
