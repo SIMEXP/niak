98a92000276f43fac6bc1984214ee6eb24f7085f
Pierre Bellec <pierre.bellec@gmail.com>
2016-07-02 16:09:20 -0400

    Added on-the-fly conversion in NIAK_CP_FMRI

--- commands/read_write/niak_cp_fmri.m
+++ commands/read_write/niak_cp_fmri.m
@@ -34,7 +34,7 @@ function [flag_error,message] = niak_cp_fmri(source,target)
 % Copyright (c) Pierre Bellec, 
 % Research Centre of the Montreal Geriatric Institute
 % & Department of Computer Science and Operations Research
-% University of Montreal, Québec, Canada, 2012.
+% University of Montreal, Qubec, Canada, 2012.
 % Maintainer : pierre.bellec@criugm.qc.ca
 % See licensing information in the code.
 % Keywords : fMRI
@@ -56,19 +56,27 @@ function [flag_error,message] = niak_cp_fmri(source,target)
 % LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 % OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 % THE SOFTWARE.
+
+% Parse folder information
 source = niak_full_file(source);
 target = niak_full_file(target);
-
-instr_copy = cat(2,'cp ',source,' ',target);
-    
-[status,msg] = system(instr_copy);
-if status~=0
-    error(msg)
-end 
-
-% Copy the "extra" file, if present
 [path_s,name_s,ext_s] = niak_fileparts(source);
 [path_t,name_t,ext_t] = niak_fileparts(target);
+
+% Copy or convert, if necessary
+if strcmp(ext_s,ext_t)
+    instr_copy = cat(2,'cp ',source,' ',target);
+    [status,msg] = system(instr_copy);
+    if status~=0
+        error(msg)
+    end 
+else
+    [hdr,vol] = niak_read_vol(source);
+    hdr.file_name = target;
+    niak_write_vol(hdr,vol);   
+end
+
+% Copy the "extra" file, if present
 file_extra_s = [path_s filesep name_s '_extra.mat'];
 file_extra_t = [path_t filesep name_t '_extra.mat'];
 if psom_exist(file_extra_s)
