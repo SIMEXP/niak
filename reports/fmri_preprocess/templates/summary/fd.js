54abcfb25159399363bf9727bf78d3948da901f6
Pierre Bellec <pierre.bellec@gmail.com>
2016-07-21 23:13:00 -0400

    Split the FD and # frames into two graphs in the preprocessing report.

--- reports/fmri_preprocess/templates/summary/fd.js
+++ reports/fmri_preprocess/templates/summary/fd.js
@@ -2,6 +2,10 @@ var dataFD= [
   ['Run'       ,'X0010002_sess1_run1' , 'X0010001_sess1_run1'],		 
   ['FD_before' , 0.22 , 0.27],
   ['FD_after'  , 0.17 , 0.15],
+];
+
+var dataNbVol= [	
+  ['Run'       ,'X0010002_sess1_run1' , 'X0010001_sess1_run1'],		 
   ['nb_vol_before'  , 120 , 85],
   ['nb_vol_after'  , 117 , 55]    
 ];
