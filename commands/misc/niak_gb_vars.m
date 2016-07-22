42749fea0dc49fbfd2398df1bfeb5731c6ac267e
Pierre-Olivier Quirion <pioliqui@gmail.com>
2016-07-22 19:36:48 -0400

    nia version is dev

--- commands/misc/niak_gb_vars.m
+++ commands/misc/niak_gb_vars.m
@@ -143,7 +143,7 @@ gb_niak_ps2pdf = 'ps2pdf';
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %% NIAK version
-gb_niak_version = '0.13.5';
+gb_niak_version = 'dev';
 
 %% Target for tests
 gb_niak_target_test = '0.14.0';
