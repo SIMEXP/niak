f7990d3d022ae139fe22e2976a34bf913cb11ee3
angela-tam <angela.tam08@gmail.com>
2016-08-04 14:44:45 -0400

    test scripts

--- extensions/subtype/test_pipeline_subtype_at.m
+++ extensions/subtype/test_pipeline_subtype_at.m
@@ -6,8 +6,8 @@ base_path = '/Users/AngelaTam/Desktop/subtype_pipeline_test/subtype_test_data/';
 pheno_path = [base_path 'numeric_pheno.csv'];
 data_path = [base_path 'raw_nii/'];
 mask_path = [base_path 'raw_nii/func_mask_group_stereonl.nii.gz'];
-subtype_path = [base_path 'pipe_out_jun9_3_sub/'];
-out_path = [base_path 'pipe_out_jun28/'];
+% subtype_path = [base_path 'pipe_out_jun9_3_sub/'];
+out_path = [base_path 'hcp_pipe_jul6_test/'];
 
 %% Configure the inputs
 pheno = niak_read_csv_cell(pheno_path);
@@ -31,19 +31,19 @@ end
 
 files_in.mask = mask_path;
 files_in.model = pheno_path;
-files_in.subtype.network_1 = [subtype_path 'network_1/network_1_subtype.mat'];
-files_in.subtype.network_2 = [subtype_path 'network_2/network_2_subtype.mat'];
-files_in.subtype.network_3 = [subtype_path 'network_3/network_3_subtype.mat'];
-files_in.subtype.network_4 = [subtype_path 'network_4/network_4_subtype.mat'];
-files_in.subtype.network_5 = [subtype_path 'network_5/network_5_subtype.mat'];
-files_in.subtype.network_6 = [subtype_path 'network_6/network_6_subtype.mat'];
-files_in.subtype.network_7 = [subtype_path 'network_7/network_7_subtype.mat'];
+% files_in.subtype.network_1 = [subtype_path 'network_1/network_1_subtype.mat'];
+% files_in.subtype.network_2 = [subtype_path 'network_2/network_2_subtype.mat'];
+% files_in.subtype.network_3 = [subtype_path 'network_3/network_3_subtype.mat'];
+% files_in.subtype.network_4 = [subtype_path 'network_4/network_4_subtype.mat'];
+% files_in.subtype.network_5 = [subtype_path 'network_5/network_5_subtype.mat'];
+% files_in.subtype.network_6 = [subtype_path 'network_6/network_6_subtype.mat'];
+% files_in.subtype.network_7 = [subtype_path 'network_7/network_7_subtype.mat'];
 
 opt.flag_test = false;
 opt.scale = 7;
 opt.folder_out = out_path;
 opt.chi2.group_col_id = 'Gender';
-opt.chi2.flag_weights = true;
+% opt.chi2.flag_weights = true;
 opt.stack.regress_conf = {'Age'};
 opt.association.contrast.Gender = 1;
 opt.visu.data_type = 'categorical';
--- extensions/subtype/test_subtyping_at.m
+++ extensions/subtype/test_subtyping_at.m
@@ -5,23 +5,22 @@ clear all
 %% set up path
 
 base_path = '/Users/AngelaTam/Desktop/subtype_pipeline_test/subtype_test_data/';
-stack_path = [base_path 'pipeline/network_stack.mat'];
+stack_path = [base_path 'hcp_pipe_jul6/network_1/network_1_stack.mat'];
 mask_path = [base_path 'func_mask_group_stereonl.nii.gz'];
-model_path = [base_path 'niak_pheno.csv'];
 out_path = [base_path 'subtyping_brick/'];
 
 %% Set up inputs
 files_in = struct;
 files_in.data = stack_path;
 files_in.mask = mask_path;
-files_in.model = model_path;
 
 files_out = struct;
+% files_out.provenance = [out_path 'pro1/provenance.mat'];
 
 opt = struct;
 opt.nb_subtype = 3;
 opt.folder_out = out_path;
-opt.flag_stats = false;
+opt.flag_prov = true;
 % opt.flag_test = true;
 
 %% Call the brick
