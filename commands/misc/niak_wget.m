779dd35b8abfcc2b2ddb2f2d7774c389c99fde88
Pierre-Olivier Quirion <pioliqui@gmail.com>
2016-08-31 00:44:27 -0400

    target in niak repo

--- commands/misc/niak_wget.m
+++ commands/misc/niak_wget.m
@@ -70,7 +70,7 @@ switch data.type;
         data.url  = ['http://www.nitrc.org/frs/download.php/9120/' data.name];
     case 'target_test_niak_mnc1'  
         data.name = [ 'target_test_niak_mnc1-' gb_niak_target_test '.zip']
-        data.url  = ['https://github.com/simexp/niak_target/archive/' data.name];
+        data.url  = ['https://github.com/simexp/niak/releases/download/' url_dir '/' data.name];
     case 'single_subject_cambridge_preprocessed_nii'
         data.name = 'single_subject_cambridge_preprocessed_nii.zip';
         data.url = 'http://www.nitrc.org/frs/download.php/6784/single_subject_cambridge_preprocessed_nii.zip';
