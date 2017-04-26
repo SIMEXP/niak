f9f2fab6197ca140dfaa72131b34fa2927818bd7
Pierre-Olivier Quirion <pioliqui@gmail.com>
2017-04-26 16:36:21 -0400

    fix to subtype blacklist

--- extensions/niak_test/niak_test_subtype_demoniak.m
+++ extensions/niak_test/niak_test_subtype_demoniak.m
@@ -108,8 +108,10 @@ if ~opt.flag_target
     out_c = [path_test.result 'report_test_regression_subtype_demoniak.csv'];
     opt_c.base_source = opt_demo.folder_out;
     opt_c.base_target = path_test.reference;
-    opt_c.black_list_source = {[opt_demo.folder_out 'logs' filesep], [opt_demo.folder_out 'provenance' filesep]};
-    opt_c.black_list_target = {[path_test.reference 'logs' filesep], [path_test.reference 'provenance' filesep]};
+    opt_c.black_list_source = {[opt_demo.folder_out 'logs' filesep] ...
+                               ,[opt_demo.folder_out 'networks' filesep 'aMPFC' filesep 'provenance_aMPFC.mat']};
+    opt_c.black_list_target = {[path_test.reference 'logs' filesep] ...
+                               ,[path_test.reference 'networks' filesep 'aMPFC' filesep 'provenance_aMPFC.mat']};
     pipeline = psom_add_job(pipeline,'test_subtype','niak_test_cmp_files',in_c,out_c,opt_c,false);
     pipeline.test_subtype.dep = list_jobs;
 end
